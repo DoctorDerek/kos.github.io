6af5533a564a3d4966924dd446384844a75fb1cb
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-09 22:18:41 -0500

    Refactor into SlideshowSlide component

--- components/Slideshow.js
+++ components/Slideshow.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState, useRef } from "react"
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
-import SlideshowSlides from "@/components/SlideshowSlides"
-import Image from "@/components/Image"
+import SlideshowSlide from "./SlideshowSlide"
 
 const Slideshow = () => {
   const [pause, setPause] = useState(false)
@@ -44,56 +43,22 @@ const Slideshow = () => {
         ref={sliderRef}
         className="grid w-full grid-cols-1 text-white border-2 border-black border-solid keen-slider"
       >
-        <div className="grid grid-cols-1 transform-gpu keen-slider__slide h-96">
-          <Image src="/images/slider/slide1.jpg" alt="" className="h-full" />
-          <div className="absolute flex flex-col items-center justify-around w-full h-full opacity-80">
-            <div className="text text1">
-              <span style={{ textShadow: "2px 2px rgba(0, 0, 0, 0.5)" }}>
-                We get you connected and keep you connected
-              </span>
-            </div>
-            <div className="text text2">
-              <strong>
-                <span style={{ textShadow: "2px 2px rgba(0, 0, 0, 0.5)" }}>
-                  worry-free connectivity
-                </span>
-              </strong>
-            </div>
-            <div className="text text3">
-              <a
-                href="/order/"
-                className="!mt-10 text-white hover:active:focus:outline-none hover:active:focus:shadow-none py-4 px-6 leading-normal h-[51px] rounded-xl align-top font-bold border-none uppercase transition-all duration-300 no-underline btn btn-orange focus:active:bg-[#005395] bg-[#b0243d] shadow-xl border:none"
-              >
-                Get connected!
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="grid grid-cols-1 transform-gpu keen-slider__slide">
-          <Image src="/images/slider/slide2.jpg" alt="" className="h-full" />
-          <div className="absolute flex flex-col items-center justify-around w-full h-full opacity-80">
-            <div className="text text1">
-              <span style={{ textShadow: "2px 2px rgba(0, 0, 0, 0.5)" }}>
-                We get you connected and keep you connected
-              </span>
-            </div>
-            <div className="text text2">
-              <strong>
-                <span style={{ textShadow: "2px 2px rgba(0, 0, 0, 0.5)" }}>
-                  worry-free connectivity
-                </span>
-              </strong>
-            </div>
-            <div className="text text3">
-              <a
-                href="/order/"
-                className="!mt-10 text-white hover:active:focus:outline-none hover:active:focus:shadow-none py-4 px-6 leading-normal h-[51px] rounded-xl align-top font-bold border-none uppercase transition-all duration-300 no-underline btn btn-orange focus:active:bg-[#005395] bg-[#b0243d] shadow-xl border:none"
-              >
-                Get connected!
-              </a>
-            </div>
-          </div>
-        </div>
+        <SlideshowSlide
+          src="/images/slider/slide1.jpg"
+          alt="ocean"
+          text1="We get you connected and keep you connected"
+          text2="worry-free connectivity"
+          href="/order/"
+          text3="Get connected!"
+        />
+        <SlideshowSlide
+          src="/images/slider/slide2.jpg"
+          alt="ocean"
+          text1="Reliable service for 100% internet enjoyment"
+          text2="worry-free connectivity"
+          href="/order/"
+          text3="Get connected!"
+        />
         {/* <canvas id="sea" /> */}
       </div>
     </>
--- components/SlideshowSlide.js
+++ components/SlideshowSlide.js
@@ -1,8 +1,45 @@
 import React from "react"
-const SlideshowSlide = ({ children }) => (
-  <div className="grid grid-cols-1 transform-gpu keen-slider__slide">
-    {children}
-  </div>
-)
+import Image from "@/components/Image"
+
+const isRequired = () => {
+  throw new Error("Missing required attribute in SlideshowSlide component")
+}
+
+const SlideshowSlide = ({
+  src = isRequired(),
+  alt = isRequired(),
+  text1 = isRequired(),
+  text2 = isRequired(),
+  href = isRequired(),
+  text3 = isRequired(),
+}) => {
+  return (
+    <div className="grid grid-cols-1 transform-gpu keen-slider__slide h-96">
+      <Image src={src} alt={alt} className="h-full" />
+      <div className="absolute flex flex-col items-center justify-around w-full h-full opacity-80">
+        <div className="text text1">
+          <span style={{ textShadow: "2px 2px rgba(0, 0, 0, 0.5)" }}>
+            {text1}
+          </span>
+        </div>
+        <div className="text text2">
+          <strong>
+            <span style={{ textShadow: "2px 2px rgba(0, 0, 0, 0.5)" }}>
+              {text2}
+            </span>
+          </strong>
+        </div>
+        <div className="text text3">
+          <a
+            href={href}
+            className="!mt-10 text-white hover:active:focus:outline-none hover:active:focus:shadow-none py-4 px-6 leading-normal h-[51px] rounded-xl align-top font-bold border-none uppercase transition-all duration-300 no-underline btn btn-orange focus:active:bg-[#005395] bg-[#b0243d] shadow-xl border:none"
+          >
+            {text3}
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default SlideshowSlide
