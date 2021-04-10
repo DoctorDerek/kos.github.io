f0fab8355f2f73b344811fd501cc03d8c1b500e8
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-09 22:57:13 -0500

    Create Header and Footer components (refactor)

--- components/Footer.js
+++ components/Footer.js
@@ -1,35 +1,152 @@
-import Link from "./Link"
-import siteMetadata from "@/data/siteMetadata"
-import SocialIcon from "@/components/social-icons"
+import Image from "@/components/Image"
 
 export default function Footer() {
   return (
     <footer>
-      <div className="flex flex-col items-center mt-16">
-        <div className="flex mb-3 space-x-4">
-          <SocialIcon
-            kind="mail"
-            href={`mailto:${siteMetadata.email}`}
-            size="6"
-          />
-          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
+      <div className="page-footer w-full h-[380px]">
+        <div className="grid w-full h-[380px]">
+          <Image src="/images/footer-bg.jpg" alt />
         </div>
-        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
+        <div className="back-to-top hidden-xs">
+          <a href="#top">
+            <span className="flex-wrap icon icon-up-arrow" />
+          </a>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+        <div className="container absolute transform-gpu left-1/2 translate-x-[-50%] translate-y-[-360px]">
+          <div className="flex flex-wrap footer-row">
+            <div className="pl-4 pr-4 sm:w-1/3">
+              <div className="location">
+                <i className="icon icon-location" />
+                The LaSalle Mews
+                <br />
+                303 Bagot Street, 16A
+                <br />
+                Kingston, ON K7K 5W7
+                <br />
+                <br />
+                <span
+                  style={{
+                    fontSize: 26,
+                    fontWeight: "bolder",
+                    color: "#c70000",
+                  }}
+                >
+                  613-549-8667
+                </span>
+              </div>
+            </div>
+            <div className="pl-4 pr-4 sm:w-1/3">
+              <div className="logo">
+                <a href="/">
+                  <div className="grid w-[300px] h-[125px]">
+                    <Image src="/images/logo-invert.png" alt="Logo" />
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="pl-4 pr-4 sm:w-1/3 first-mobile">
+              <div className="location">
+                <i className="icon icon-location" />
+                183 Front Street
+                <br />
+                (near City Hall)
+                <br />
+                Belleville, ON K8N 2Y9 <br />
+                <br />
+                <span
+                  style={{
+                    fontSize: 26,
+                    fontWeight: "bolder",
+                    color: "#c70000",
+                  }}
+                >
+                  613-968-7137
+                </span>
+              </div>
+            </div>{" "}
+          </div>
+          <div className="footer-bot">
+            <div>
+              <div id="plemx-root" />
+              <a href="http://www.theweathernetwork.com">The Weather Network</a>
+            </div>
+            <div className="copyright">
+              Copyright © KOS - 2021 {" | "}
+              <a style={{ color: "#ffffff" }} href="/policies/">
+                KOS Policies
+              </a>
+              {" | "}
+              <a style={{ color: "#ffffff" }} href="/tac/">
+                Terms and Conditions
+              </a>
+              {" | "}
+              <a style={{ color: "#ffffff" }} href="/e911">
+                e911
+              </a>
+              {" | "}
+              <a style={{ color: "#ffffff" }} href="/wst/">
+                Personal Web Space TAC
+              </a>
+              <br />
+              <div align="center">
+                <span style={{ color: "#ffffff" }}>
+                  <strong>Serving Eastern Ontario: </strong>
+                </span>
+                <a
+                  style={{ color: "#fff" }}
+                  href="/kingston-internet-services/"
+                >
+                  Kingston
+                </a>
+                {" | "}
+                <a
+                  style={{ color: "#fff" }}
+                  href="/gananoque-internet-services/"
+                >
+                  Gananoque
+                </a>
+                {" | "}
+                <a style={{ color: "#fff" }} href="/picton-internet-services/">
+                  Picton
+                </a>
+                {" | "}
+                <a
+                  style={{ color: "#fff" }}
+                  href="/brighton-internet-services/"
+                >
+                  Brighton
+                </a>
+                {" | "}
+                <a style={{ color: "#fff" }} href="/trenton-internet-services/">
+                  Trenton
+                </a>
+                {" | "}
+                <a
+                  style={{ color: "#fff" }}
+                  href="/belleville-internet-services/"
+                >
+                  Belleville{" "}
+                </a>
+                {" | "}
+                <a style={{ color: "#fff" }} href="/napanee-internet-services/">
+                  Napanee
+                </a>
+              </div>
+            </div>
+            <div className="social-links">
+              <ul>
+                <li>
+                  <a
+                    className="icon icon-social-facebook"
+                    href="https://www.facebook.com/Kingston-Online-Services-120301901358202/"
+                    target="_blank"
+                  />
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+        <canvas id="footerSea" />
       </div>
     </footer>
   )
