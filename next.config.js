aca03f0ad6e4bf28c8854a11457e2122344a1e1b
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-11 19:03:47 -0500

    Update next.config to remove experimental feature & use webpack 5; also comment out JavaScript file loading (currently implemented with useScript)

--- next.config.js
+++ next.config.js
@@ -2,43 +2,10 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 })
 
-const fs = require("fs")
-
 module.exports = withBundleAnalyzer({
-  env: {
-    googleAnalytics: fs
-      .readFileSync("./public/js/googleAnalytics.js")
-      .toString(),
-    jquery: fs.readFileSync("./public/js/jquery.js").toString(),
-    bootstrap: fs
-      .readFileSync("./public/js/plugins/bootstrap.min.js")
-      .toString(),
-    nivo: fs
-      .readFileSync("./public/js/plugins/jquery.nivo.slider.js")
-      .toString(),
-    slick: fs.readFileSync("./public/js/plugins/slick.min.js").toString(),
-    magnific: fs
-      .readFileSync("./public/js/plugins/jquery.magnific-popup.min.js")
-      .toString(),
-    waypoints: fs
-      .readFileSync("./public/js/plugins/jquery.waypoints.min.js")
-      .toString(),
-    count: fs.readFileSync("./public/js/plugins/jquery.countTo.js").toString(),
-    form: fs.readFileSync("./public/js/plugins/jquery.form.js").toString(),
-    validate: fs
-      .readFileSync("./public/js/plugins/jquery.validate.min.js")
-      .toString(),
-    custom: fs.readFileSync("./public/js/custom.js").toString(),
-    accessibility: fs
-      .readFileSync("./public/js/accessibilitymenu.js")
-      .toString(),
-    theWeatherNetwork: fs
-      .readFileSync("./public/js/theWeatherNetwork.js")
-      .toString(),
-  },
   pageExtensions: ["js", "jsx", "md", "mdx"],
-  experimental: {
-    modern: true,
+  future: {
+    webpack5: true,
   },
   webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
@@ -71,3 +38,38 @@ module.exports = withBundleAnalyzer({
     return config
   },
 })
+
+/*
+module.exports = withBundleAnalyzer({
+  env: {
+    googleAnalytics: fs
+      .readFileSync("./public/js/googleAnalytics.js")
+      .toString(),
+    jquery: fs.readFileSync("./public/js/jquery.js").toString(),
+    bootstrap: fs
+      .readFileSync("./public/js/plugins/bootstrap.min.js")
+      .toString(),
+    nivo: fs
+      .readFileSync("./public/js/plugins/jquery.nivo.slider.js")
+      .toString(),
+    slick: fs.readFileSync("./public/js/plugins/slick.min.js").toString(),
+    magnific: fs
+      .readFileSync("./public/js/plugins/jquery.magnific-popup.min.js")
+      .toString(),
+    waypoints: fs
+      .readFileSync("./public/js/plugins/jquery.waypoints.min.js")
+      .toString(),
+    count: fs.readFileSync("./public/js/plugins/jquery.countTo.js").toString(),
+    form: fs.readFileSync("./public/js/plugins/jquery.form.js").toString(),
+    validate: fs
+      .readFileSync("./public/js/plugins/jquery.validate.min.js")
+      .toString(),
+    custom: fs.readFileSync("./public/js/custom.js").toString(),
+    accessibility: fs
+      .readFileSync("./public/js/accessibilitymenu.js")
+      .toString(),
+    theWeatherNetwork: fs
+      .readFileSync("./public/js/theWeatherNetwork.js")
+      .toString(),
+  },
+*/
