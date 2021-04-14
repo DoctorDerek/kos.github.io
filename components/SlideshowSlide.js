582744edfda05efc967be03f6bbd125ad094ee91
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-13 19:12:59 -0500

    Remove janky animations to improve CLS

--- components/SlideshowSlide.js
+++ components/SlideshowSlide.js
@@ -22,22 +22,22 @@ const SlideshowSlide = ({
     >
       <Image src={src} alt={alt} className="h-full" />
       <div className="absolute flex flex-col items-center justify-center w-full h-full opacity-80">
-        <div className="text text1">
+        <div className="text text1 transform-gpu">
           <span style={{ textShadow: "2px 2px rgba(0, 0, 0, 0.5)" }}>
             {text1}
           </span>
         </div>
-        <div className="text text2">
+        <div className="text text2 transform-gpu">
           <strong>
             <span style={{ textShadow: "2px 2px rgba(0, 0, 0, 0.5)" }}>
               {text2}
             </span>
           </strong>
         </div>
-        <div className="text text3">
+        <div className="text text3 transform-gpu">
           <button
             href={href}
-            className="!mt-10 text-white hover:active:focus:outline-none hover:active:focus:shadow-none py-4 px-6 leading-normal h-[51px] rounded-xl align-top font-bold border-none uppercase transition-all transform-gpu duration-300 no-underline btn btn-orange focus:active:bg-[#005395] bg-[#b0243d] shadow-xl border:none"
+            className="!mt-10 text-white hover:active:focus:outline-none hover:active:focus:shadow-none py-4 px-6 leading-normal h-[51px] rounded-xl align-top font-bold border-none uppercase no-underline btn btn-orange focus:active:bg-[#005395] bg-[#b0243d] shadow-xl border:none"
           >
             {text3}
           </button>
