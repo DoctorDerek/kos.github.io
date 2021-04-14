5f88113f6734276156d25fec70ecc83a38d4ce41
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-14 00:23:01 -0500

    Remove typo border:none

--- components/SlideshowSlide.js
+++ components/SlideshowSlide.js
@@ -37,7 +37,7 @@ const SlideshowSlide = ({
         <div className="text text3">
           <button
             href={href}
-            className="!mt-10 text-white hover:active:focus:outline-none hover:active:focus:shadow-none py-4 px-6 leading-normal h-[51px] rounded-xl align-top font-bold border-none uppercase no-underline btn btn-orange focus:active:bg-[#005395] bg-[#b0243d] shadow-xl border:none"
+            className="!mt-10 text-white hover:active:focus:outline-none hover:active:focus:shadow-none py-4 px-6 leading-normal h-[51px] rounded-xl align-top font-bold border-none uppercase no-underline btn btn-orange focus:active:bg-[#005395] bg-[#b0243d] shadow-xl"
           >
             {text3}
           </button>
