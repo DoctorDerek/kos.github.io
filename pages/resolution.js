5fef2bc0423157aa9f08f9393676a493857e823d
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-15 04:51:59 -0500

    Fix minor issues causing build problems

--- pages/resolution.js
+++ pages/resolution.js
@@ -136,7 +136,7 @@ export default function ResolutionServices() {
                           className="input-custom input-full"
                           name="name"
                           placeholder="Your name"
-                          style='background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAfBJREFUWAntVk1OwkAUZkoDKza4Utm61iP0AqyIDXahN2BjwiHYGU+gizap4QDuegWN7lyCbMSlCQjU7yO0TOlAi6GwgJc0fT/fzPfmzet0crmD7HsFBAvQbr /crw+Gw5fu+AfOYvgylJ4TwCoVCs1ardYTruqfj8fgV5OUMSVVT93VdP9dAzpVvm5wJHZFbg2LQ2pEYOlZ/oiDvwNcsFoseY4PBwMCrhaeCJyKWZU37KOJcYdi27QdhcuuBIb073BvTNL8ln4NeeR6NRi/wxZKQcGurQs5oNhqLshzVTMBewW/LMU3TTNlO0ieTiStjYhUIyi6DAp0xbEdgTt+LE0aCKQw24U4llsCs4ZRJrYopB6RwqnpA1YQ5NGFZ1YQ41Z5S8IQQdP5laEbr /JcD4Vj5DEsW2gE6s6g3d/YP/g+BDnT7GNi2qCjTwGd6riBzHaaCEd3Js01vwCPIbmWbr /x1nwAN/1ov+/drgFWIlfKpVukyYihtgkXNp4mABK+1GtVr+SBhJDbBIubVw+Cd/TDgKO2DPiN3YUo6y/nDCNEIsqTKH1en2tcwA9FKEItyDi3aIh8Gl1sRrVnSDzNFDJT1bAy5xpOYGn5fP5JuL95ZjMIn1ya7j5dPGfv0A5eAnpZUY3n5jXcoec5J67D9q+VuAPM47D3XaSeL4AAAAASUVORK5CYII="); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;'
+                          style={undefined}
                         />
                       </div>
                     </div>
--- pages/wst.js
+++ pages/wst.js
@@ -1,81 +1,84 @@
 export default function PersonalWebSpaceTAC() {
-  ;<div className="container">
-    <h1 className="text-center decor color">
-      KOS Personal Web Space Terms &amp; Conditions
-    </h1>
-    <p>
-      <span style={undefined}>
-        This document is an addendum to our other service agreement terms and
-        conditions as well as our acceptable use policy for Internet
-        connectivity with Kingston Online Services.
-      </span>
-    </p>
-    <p className="text-left font18">
-      <strong>Additional Policies</strong>
-    </p>
+  return (
+    <div className="container">
+      <h1 className="text-center decor color">
+        KOS Personal Web Space Terms &amp; Conditions
+      </h1>
+      <p>
+        <span style={undefined}>
+          This document is an addendum to our other service agreement terms and
+          conditions as well as our acceptable use policy for Internet
+          connectivity with Kingston Online Services.
+        </span>
+      </p>
+      <p className="text-left font18">
+        <strong>Additional Policies</strong>
+      </p>
 
-    <p className="text-left font18">
-      <strong>1. Limitations of Services</strong>
-    </p>
+      <p className="text-left font18">
+        <strong>1. Limitations of Services</strong>
+      </p>
 
-    <p className="text-left font18"></p>
-    <ol>
-      <li>
-        The KOS Member web pages are meant as personal web pages, not as
-        commercial web pages. All information must be of a personal nature, and
-        may not be commercial in nature or content. Non-profit organizations are
-        generally also considered to be commercial in nature.
-      </li>
-      <li>
-        Using any web pages for high volume or commercial use may incur
-        additional fees or result in the removal of the material and/or access
-        to this service.
-      </li>
-      <li>
-        Linking an image, animation or other file for display on another web
-        site is strictly prohibited, whether for profit or personal use.
-        Kingston Online reserves the right to limit this activity with
-        filtering.
-      </li>
-      <li>
-        Technical support for the “Member Web Pages” is limited to basic setup
-        of the account, and questions related to directory, file placement,
-        suitability and naming conventions. Kingston Online Services will not
-        provide technical support for HTML design, graphic design or any other
-        factor in the creation of web content.
-      </li>
-    </ol>
+      <p className="text-left font18"></p>
+      <ol>
+        <li>
+          The KOS Member web pages are meant as personal web pages, not as
+          commercial web pages. All information must be of a personal nature,
+          and may not be commercial in nature or content. Non-profit
+          organizations are generally also considered to be commercial in
+          nature.
+        </li>
+        <li>
+          Using any web pages for high volume or commercial use may incur
+          additional fees or result in the removal of the material and/or access
+          to this service.
+        </li>
+        <li>
+          Linking an image, animation or other file for display on another web
+          site is strictly prohibited, whether for profit or personal use.
+          Kingston Online reserves the right to limit this activity with
+          filtering.
+        </li>
+        <li>
+          Technical support for the “Member Web Pages” is limited to basic setup
+          of the account, and questions related to directory, file placement,
+          suitability and naming conventions. Kingston Online Services will not
+          provide technical support for HTML design, graphic design or any other
+          factor in the creation of web content.
+        </li>
+      </ol>
 
-    <p></p>
+      <p></p>
 
-    <p className="text-left font18">
-      <strong>2. Suitability &amp; Standards</strong>
-    </p>
+      <p className="text-left font18">
+        <strong>2. Suitability &amp; Standards</strong>
+      </p>
 
-    <p className="text-left font18">
-      The content of web pages must meet with the standards of Kingston Online
-      Services. We expect all of our clients to respect these standards when
-      providing information via our KOS Member Pages over the Internet.
-    </p>
-    <ol>
-      <li>
-        All web pages must conform to Canadian broadcast standards. They must
-        meet or exceed provincial or federal laws and respect all copyright
-        restrictions.
-      </li>
-      <li>
-        Content which is pornographic is strictly prohibited. Nudity is not
-        pornography, but in cases where Kingston Online Services is called upon
-        to adjudicate, any nudity will be removed.
-      </li>
-      <li>
-        Content must not exploit, discriminate against, or derogatorize race,
-        sex, creed, religion, disability, or any other part of a person’s human
-        or civil rights. This extends to groups and gatherings of people of like
-        mind.
-      </li>
-    </ol>
+      <p className="text-left font18">
+        The content of web pages must meet with the standards of Kingston Online
+        Services. We expect all of our clients to respect these standards when
+        providing information via our KOS Member Pages over the Internet.
+      </p>
+      <ol>
+        <li>
+          All web pages must conform to Canadian broadcast standards. They must
+          meet or exceed provincial or federal laws and respect all copyright
+          restrictions.
+        </li>
+        <li>
+          Content which is pornographic is strictly prohibited. Nudity is not
+          pornography, but in cases where Kingston Online Services is called
+          upon to adjudicate, any nudity will be removed.
+        </li>
+        <li>
+          Content must not exploit, discriminate against, or derogatorize race,
+          sex, creed, religion, disability, or any other part of a person’s
+          human or civil rights. This extends to groups and gatherings of people
+          of like mind.
+        </li>
+      </ol>
 
-    <p></p>
-  </div>
+      <p></p>
+    </div>
+  )
 }
