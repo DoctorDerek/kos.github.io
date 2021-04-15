77412f19e790c10d1d7f144ee7df8f904367175f
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-15 01:34:14 -0500

    Create business.js

---
+++ pages/business.js
@@ -0,0 +1,131 @@
+export default function BusinessServices() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="divider-lg visible-xs"></div>
+        <div class="col-md-8 col-lg-9">
+          <h1 class="text-center decor color">Business Services</h1>
+
+          <div
+            id="page-content"
+            style="position: relative; margin-top: 90px; color: #000000; font-family: 'Open Sans', sans-serif; font-size: 15px; left: 0px !important;"
+          >
+            <div class="block" style="margin-bottom: 90px;">
+              <div
+                class="container"
+                style="padding-right: 15px; padding-left: 15px; margin-right: auto; margin-left: auto; width: 1200px;"
+              >
+                <div
+                  class="row"
+                  style="margin-right: -15px; margin-left: -15px;"
+                >
+                  <div
+                    class="col-md-8 col-lg-9"
+                    style="position: relative; min-height: 1px; padding-right: 15px; padding-left: 15px; float: left; width: 900px;"
+                  >
+                    <p style="margin-top: 0px; margin-bottom: 22px; line-height: 22px; color: #000000; background-color: #ffffff;">
+                      <span style="color: #000000; background-color: #ffffff;">
+                        Kingston Online Services offers Internet for your
+                        business at speeds starting from dial-up connectivity to
+                        high speed services intended for video and telephony.
+                        Kingston Online Services also provides innovative
+                        solutions for rural connectivity where conventional
+                        Internet services are not available.
+                      </span>
+                    </p>
+                    <h5
+                      class="color"
+                      style="font-family: Lato, sans-serif; font-weight: bold; line-height: 1.273em; color: #005395; margin-top: 0px; margin-bottom: 20px; font-size: 22px; text-transform: uppercase; background-color: #ffffff;"
+                    >
+                      PLEASE CONTACT OUR OFFICE FOR MORE INFORMATION, OR SELECT
+                      FROM ONE OF OUR SERVICES BELOW.
+                    </h5>
+                    <div
+                      class="row"
+                      style="margin-right: -15px; margin-left: -15px; color: #000000; background-color: #ffffff;"
+                    >
+                      <div
+                        class="col-sm-6"
+                        style="position: relative; min-height: 1px; padding-right: 15px; padding-left: 15px; float: left; width: 450px;"
+                      >
+                        <ul
+                          class="marker-list pad-left"
+                          style="margin-top: 25px; margin-bottom: 0px; margin-left: 0px; padding-left: 28px; font-size: 16px; line-height: 22px; list-style-type: none; list-style-position: initial;"
+                        >
+                          <li style="padding-bottom: 10px; padding-left: 30px; position: relative;">
+                            <a
+                              style="color: #000000; text-decoration-line: none;"
+                              href="https://www.kos.net/bus/high-speed-cable/"
+                            >
+                              High Speed Cable
+                            </a>
+                          </li>
+                          <li style="padding-bottom: 10px; padding-left: 30px; position: relative;">
+                            <a
+                              style="color: #000000; text-decoration-line: none;"
+                              href="https://www.kos.net/bus/wireless-broadband/"
+                            >
+                              Wireless Broadband
+                            </a>
+                          </li>
+                          <li style="padding-bottom: 10px; padding-left: 30px; position: relative;">
+                            <a
+                              style="color: #000000; text-decoration-line: none;"
+                              href="https://www.kos.net/bus/high-speed-dsl/"
+                            >
+                              High Speed DSL
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div
+                        class="col-sm-6"
+                        style="position: relative; min-height: 1px; padding-right: 15px; padding-left: 15px; float: left; width: 450px;"
+                      >
+                        <ul
+                          class="marker-list pad-left"
+                          style="margin-top: 25px; margin-bottom: 0px; margin-left: 0px; padding-left: 28px; font-size: 16px; line-height: 22px; list-style-type: none; list-style-position: initial;"
+                        >
+                          <li style="padding-bottom: 10px; padding-left: 30px; position: relative;">
+                            <a
+                              style="color: #000000; text-decoration-line: none;"
+                              href="https://www.kos.net/bus/voip/"
+                            >
+                              VoIP
+                            </a>
+                          </li>
+                          <li style="padding-bottom: 10px; padding-left: 30px; position: relative;">
+                            <a
+                              style="color: #000000; text-decoration-line: none;"
+                              href="https://www.kos.net/bus/dial-up/"
+                            >
+                              Dial Up
+                            </a>
+                          </li>
+                          <li style="padding-bottom: 10px; padding-left: 30px; position: relative;">
+                            <a
+                              style="color: #000000; text-decoration-line: none;"
+                              href="https://www.kos.net/bus/mail/"
+                            >
+                              Mail
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <a href="/order/" class="btn btn-orange">
+              Get Connected
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
