aae4d006bd8b08064a04b3e2bf7a29e3efea3dbd
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-15 01:30:26 -0500

    Reorganize pages

--- pages/campgrounds.js
+++ pages/campgrounds.js
@@ -1,5 +1,217 @@
-import React from "react"
+export default function Campgrounds() {
+  return (
+    <div className="container">
+      <h1 className="text-center decor color">Campgrounds</h1>
 
-export default function index() {
-  return <div>Hallo /campgrounds</div>
+      <p
+        className="text-center font18"
+        style="margin-top: 0px; margin-bottom: 26px; font-size: 18px; line-height: 26px; text-align: center; color: #000000; font-family: 'Open Sans', sans-serif; background-color: #ffffff;"
+      >
+        <span style="color: #c70000; font-weight: bold;">
+          KOS is now offering Internet service at the following Campgrounds:
+        </span>
+      </p>
+      <p
+        style="margin-top: 0px; margin-bottom: 22px; font-size: 15px; line-height: 22px; color: #000000; font-family: 'Open Sans', sans-serif; background-color: #ffffff;"
+        align="center"
+      >
+        <span style="color: #c70000; font-weight: bold;">•</span>
+        <span style="font-size: 18px; font-weight: bold;">
+          Quinte’s Isle Campark
+        </span>
+        <br />
+        <span style="color: #c70000; font-weight: bold;">•</span>
+        <span style="font-size: 18px; font-weight: bold;">Wesley Acres</span>
+        <br />
+        <span style="color: #c70000; font-weight: bold;">•</span>
+        <span style="font-size: 18px; font-weight: bold;">Edgewater</span>
+      </p>
+      <p
+        className="text-center font18"
+        style="margin-top: 0px; margin-bottom: 26px; font-size: 18px; line-height: 26px; text-align: center; color: #000000; font-family: 'Open Sans', sans-serif; background-color: #ffffff;"
+      >
+        <span style="color: #c70000; font-weight: bold;">
+          KOS now offers 4 great Wi-Fi Packages
+        </span>
+        <br />
+        Simply look for the campground network on your wireless compatible
+        device when you are at the park and self-register for the Wi-Fi package
+        of your choice.
+      </p>
+      <div
+        className="row"
+        style="margin-right: -15px; margin-left: -15px; color: #000000; font-family: 'Open Sans', sans-serif; font-size: 15px; background-color: #ffffff;"
+      >
+        <div
+          className="col-sm-6 col-md-3"
+          style="position: relative; min-height: 1px; padding-right: 15px; padding-left: 15px; float: left; width: 300px;"
+        >
+          <div
+            className="contact-box"
+            style="padding: 18px 10px 10px; margin-bottom: 35px; background-image: linear-gradient(#27bae9 0%, #0a6eab 100%); background-repeat: initial; border-radius: 8px;"
+          >
+            <div
+              className="contact-box-title"
+              style="font-size: 22px; line-height: 24px; font-weight: bold; margin-bottom: 14px; color: #ffffff; text-transform: uppercase; font-family: Lato, sans-serif;"
+            >
+              DAILY*
+            </div>
+            <div
+              className="contact-box-row"
+              style="background-color: #ffffff; position: relative; font-size: 14px; line-height: 22px; color: #000000; padding: 16px 16px 16px 52px; margin-top: 4px; border-radius: 4px;"
+            >
+              <div
+                className="contact-box-row-title"
+                style="margin-bottom: 2px; font-size: 16px; line-height: 20px; font-weight: 600; font-family: Lato, sans-serif; color: #005395; text-transform: uppercase;"
+              >
+                <span style="font-size: 28px; font-weight: bold;">$7.95</span>
+              </div>
+              <div>plus tax</div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-sm-6 col-md-3"
+          style="position: relative; min-height: 1px; padding-right: 15px; padding-left: 15px; float: left; width: 300px;"
+        >
+          <div
+            className="contact-box"
+            style="padding: 18px 10px 10px; margin-bottom: 35px; background-image: linear-gradient(#27bae9 0%, #0a6eab 100%); background-repeat: initial; border-radius: 8px;"
+          >
+            <div
+              className="contact-box-title"
+              style="font-size: 22px; line-height: 24px; font-weight: bold; margin-bottom: 14px; color: #ffffff; text-transform: uppercase; font-family: Lato, sans-serif;"
+            >
+              3 DAY WEEKEND*
+            </div>
+            <div
+              className="contact-box-row"
+              style="background-color: #ffffff; position: relative; font-size: 14px; line-height: 22px; color: #000000; padding: 16px 16px 16px 52px; margin-top: 4px; border-radius: 4px;"
+            >
+              <div
+                className="contact-box-row-title"
+                style="margin-bottom: 2px; font-size: 16px; line-height: 20px; font-weight: 600; font-family: Lato, sans-serif; color: #005395; text-transform: uppercase;"
+              >
+                <span style="font-size: 28px; font-weight: bold;">$14.95</span>
+              </div>
+              <div>plus tax</div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-sm-6 col-md-3"
+          style="position: relative; min-height: 1px; padding-right: 15px; padding-left: 15px; float: left; width: 300px;"
+        >
+          <div
+            className="contact-box"
+            style="padding: 18px 10px 10px; margin-bottom: 35px; background-image: linear-gradient(#27bae9 0%, #0a6eab 100%); background-repeat: initial; border-radius: 8px;"
+          >
+            <div
+              className="contact-box-title"
+              style="font-size: 22px; line-height: 24px; font-weight: bold; margin-bottom: 14px; color: #ffffff; text-transform: uppercase; font-family: Lato, sans-serif;"
+            >
+              WEEKLY*
+            </div>
+            <div
+              className="contact-box-row"
+              style="background-color: #ffffff; position: relative; font-size: 14px; line-height: 22px; color: #000000; padding: 16px 16px 16px 52px; margin-top: 4px; border-radius: 4px;"
+            >
+              <div
+                className="contact-box-row-title"
+                style="margin-bottom: 2px; font-size: 16px; line-height: 20px; font-weight: 600; font-family: Lato, sans-serif; color: #005395; text-transform: uppercase;"
+              >
+                <span style="font-size: 28px; font-weight: bold;">$26.95</span>
+              </div>
+              <div>plus tax</div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-sm-6 col-md-3"
+          style="position: relative; min-height: 1px; padding-right: 15px; padding-left: 15px; float: left; width: 300px;"
+        >
+          <div
+            className="contact-box"
+            style="padding: 18px 10px 10px; margin-bottom: 35px; background-image: linear-gradient(#27bae9 0%, #0a6eab 100%); background-repeat: initial; border-radius: 8px;"
+          >
+            <div
+              className="contact-box-title"
+              style="font-size: 22px; line-height: 24px; font-weight: bold; margin-bottom: 14px; color: #ffffff; text-transform: uppercase; font-family: Lato, sans-serif;"
+            >
+              MONTHLY**
+            </div>
+            <div
+              className="contact-box-row"
+              style="background-color: #ffffff; position: relative; font-size: 14px; line-height: 22px; color: #000000; padding: 16px 16px 16px 52px; margin-top: 4px; border-radius: 4px;"
+            >
+              <div
+                className="contact-box-row-title"
+                style="margin-bottom: 2px; font-size: 16px; line-height: 20px; font-weight: 600; font-family: Lato, sans-serif; color: #005395; text-transform: uppercase;"
+              >
+                <span style="font-size: 28px; font-weight: bold;">$46.95</span>
+              </div>
+              <div>plus tax</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p
+        className="text-center font18"
+        style="margin-top: 0px; margin-bottom: 26px; font-size: 18px; line-height: 26px; text-align: center; color: #000000; font-family: 'Open Sans', sans-serif; background-color: #ffffff;"
+      >
+        Credit Card required * Connection limited to one device ** Connection
+        limited to two devices – Please&nbsp;
+        <a style="color: #005395;" href="https://www.kos.net/contact.php">
+          call to activate
+        </a>
+        &nbsp;second device
+      </p>
+      <p
+        className="text-center font18"
+        style="margin-top: 0px; margin-bottom: 26px; font-size: 18px; line-height: 26px; text-align: center; color: #000000; font-family: 'Open Sans', sans-serif; background-color: #ffffff;"
+      >
+        &nbsp;
+      </p>
+      <div
+        className="contact-box"
+        style="padding: 18px 10px 10px; margin-bottom: 35px; background-image: linear-gradient(#27bae9 0%, #0a6eab 100%); background-repeat: initial; background-color: #ffffff; border-radius: 8px; color: #000000; font-family: 'Open Sans', sans-serif; font-size: 15px;"
+      >
+        <div
+          className="contact-box-title"
+          style="font-size: 22px; line-height: 24px; font-weight: bold; margin-bottom: 14px; color: #ffffff; text-transform: uppercase; font-family: Lato, sans-serif;"
+        >
+          SEASONAL FIXED WIRELESS INTERNET SERVICE
+        </div>
+        <div
+          className="contact-box-row"
+          style="background-color: #ffffff; position: relative; font-size: 14px; line-height: 22px; color: #000000; padding: 16px 16px 16px 52px; margin-top: 4px; border-radius: 4px;"
+        >
+          <div
+            className="contact-box-row-title"
+            style="margin-bottom: 2px; font-size: 16px; line-height: 20px; font-weight: 600; font-family: Lato, sans-serif; color: #005395; text-transform: uppercase;"
+          >
+            <span style="font-size: 28px; font-weight: bold;">$59.99</span>
+          </div>
+          <div>
+            <ul style="margin-bottom: 10px;">
+              <li>250 GB per month, up to 6 Mbps</li>
+              <li>
+                Friendly technical support 7 days a week. Seasonal billing
+                options are available.
+              </li>
+              <li>
+                <a
+                  style="color: #005395;"
+                  href="https://www.kos.net/contact.php"
+                >
+                  Contact us today
+                </a>
+                &nbsp;and ask about our free installation
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
