f0ba059fa51b99bd0a16a5e0ea0a733b24ee8d95
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-15 03:02:58 -0500

    Update header phone icon to match design better

--- components/Header.js
+++ components/Header.js
@@ -35,12 +35,8 @@ export default function Header() {
         <Column>
           <div className="text-xs font-normal text-right text-blue-800 uppercase">
             <div className="flex mb-1 text-3xl leading-8 text-gray-700">
-              <div className="grid self-center w-8 h-8 p-1 border-2 border-black border-solid rounded-full">
-                <Image
-                  src="/assets/material-icons/phone_black_48dp.svg"
-                  alt=""
-                />
-                {/* alt="" for purely decorative icon */}
+              <div className="grid self-center w-8 h-8 p-1 border-2 border-black border-solid rounded-full fill-current">
+                <PhoneIcon />
               </div>
               <div>
                 <span className="text-[20px]">
