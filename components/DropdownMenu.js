daa01c09828dbd1fa3ef44615fd597f7feda8c94
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-23 18:03:06 -0500

    Switch to Tailwind UI template for flyout menu

--- components/DropdownMenu.js
+++ components/DropdownMenu.js
@@ -1,11 +1,12 @@
+import { Fragment } from "react"
+import { Popover, Transition } from "@headlessui/react"
+import { ChevronDownIcon } from "@heroicons/react/solid"
 import useDropdownMenu from "react-accessible-dropdown-menu-hook"
 import Link from "@/components/Link"
 import DownArrow from "@/data/material-icons/keyboard_arrow_down_black_48dp.svg"
 import LeftArrow from "@/data/material-icons/keyboard_arrow_left_black_48dp.svg"
 import RightArrow from "@/data/material-icons/keyboard_arrow_right_black_48dp.svg"
 import UpArrow from "@/data/material-icons/keyboard_arrow_up_black_48dp.svg"
-
-import { useRouter } from "next/router"
 
 const NAVIGATION_MENU_MAP = new Map([
   ["Home", "/"],
@@ -35,17 +36,6 @@ const NAVIGATION_MENU_MAP = new Map([
           ["Mail", "/home-internet-in-kingston-ontario/mail"],
           ["Dial Up", "/home-internet-in-kingston-ontario/dial-up"],
         ]),
-        /*
-        new Map([
-          ["Residential Internet", "/residential/"],
-          ["High Speed Cable", "/res/high-speed-cable/"],
-          ["Wireless Broadband", "/res/wireless-broadband/"],
-          ["High Speed DSL", "/res/high-speed-dsl/"],
-          ["VoIP", "/res/voip/"],
-          ["Mail", "/res/mail/"],
-          ["Dial Up", "/res/dial-up/"],
-        ]),
-        */
       ],
       [
         "Business",
@@ -101,145 +91,98 @@ const NAVIGATION_MENU_MAP = new Map([
   ],
 ])
 
-const isRequired = () => {
-  throw new Error(
-    "Missing required argument navigationMenu or currentPath in DropdownMenu"
-  )
+const solutions = [
+  {
+    name: "Blog",
+    description: "Learn about tips, product updates and company culture.",
+    href: "#",
+  },
+  {
+    name: "Help Center",
+    description:
+      "Get all of your questions answered in our forums of contact support.",
+    href: "#",
+  },
+  {
+    name: "Guides",
+    description:
+      "Learn how to maximize our platform to get the most out of it.",
+    href: "#",
+  },
+  {
+    name: "Events",
+    description:
+      "Check out webinars with experts and learn about our annual conference.",
+    href: "#",
+  },
+  {
+    name: "Security",
+    description: "Understand how we take your privacy seriously.",
+    href: "#",
+  },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
 }
 
-const makeNavigationMenu = (
-  navigationMenu = isRequired(),
-  currentPath = isRequired(),
-  dropdownMenuItemProps = null
-) => {
-  const containsCurrentPage = (menuToSearch) => {
-    if (typeof menuToSearch === "string") {
-      return currentPath === menuToSearch
-    }
-    return Array.from(menuToSearch).some(([text, href]) => {
-      if (typeof href === "string") {
-        // this is an actual link
-        console.log(currentPath, href)
-        return currentPath === href
-      }
-      // we need to search this submenu
-      return containsCurrentPage(href)
-    })
-  }
-
-  return [...navigationMenu.entries()].map(
-    ([text, destinationOrSubmenu], itemIndex) => {
-      if (typeof destinationOrSubmenu === "string") {
-        // we don't have a submenu, just a URL
-        const href = destinationOrSubmenu
-        let childProps = {}
-        if (dropdownMenuItemProps && dropdownMenuItemProps[itemIndex]) {
-          // pass in itemProps from react-accessible-dropdown-menu-hook
-          childProps = { ...dropdownMenuItemProps[itemIndex] }
-        }
-        childProps.key = text + href
-
-        const isCurrentPageProps = containsCurrentPage(href)
-          ? {
-              "aria-current": "page",
-              className: "text-blue-800",
-            }
-          : {}
-
-        return (
-          <li {...childProps} {...isCurrentPageProps}>
-            <Link href={href} className="block">
-              {text}
-            </Link>
-          </li>
-        )
-      }
-      if (
-        destinationOrSubmenu !== null &&
-        typeof destinationOrSubmenu === "object"
-      ) {
-        // we have a submenu to populate
-        const submenu = destinationOrSubmenu
-        const numberOfItems = submenu.length
-        const { buttonProps, itemProps, isOpen, setIsOpen } = useDropdownMenu(
-          numberOfItems
-        )
-        let ArrowIcon
-        if (dropdownMenuItemProps) {
-          // we're inside a sub-menu
-          if (isOpen) {
-            ArrowIcon = <LeftArrow />
-          } else {
-            ArrowIcon = <RightArrow />
-          }
-        } else {
-          // we're a top-level menu
-          if (isOpen) {
-            ArrowIcon = <UpArrow />
-          } else {
-            ArrowIcon = <DownArrow />
-          }
-        }
-        const uniqueKey = text + "Menu"
-
-        return (
-          <li
-            className="relative dropdown"
-            onMouseEnter={() => setIsOpen(true)}
-            onMouseLeave={() => setIsOpen(false)}
-            key={uniqueKey}
-          >
-            <button
-              {...buttonProps}
-              aria-controls={uniqueKey}
-              aria-expanded={isOpen}
-              className={
-                "flex w-full" +
-                (containsCurrentPage(submenu) ? " text-blue-800" : "")
-              }
-            >
-              {text}
-              {String}
-              <div className="grid self-center w-6 h-6 rounded-full fill-current">
-                {ArrowIcon}
-              </div>
-            </button>
-            <ul
-              className={
-                "absolute z-50 bg-white dropdown-menu " +
-                (isOpen ? "visible" : "hidden")
-              }
-              id={uniqueKey}
-            >
-              <li className="dropdown-submenu">
-                {makeNavigationMenu(submenu, itemProps)}
-              </li>
-            </ul>
-          </li>
-        )
-      }
-      throw new Error("Unknown destinationOrSubmenu prop in DropdownMenu")
-    }
-  )
-}
-
-export default function DropdownMenu() {
-  const router = useRouter() // next/router
-
+export default function Example() {
   return (
-    <nav
-      className="navbar"
-      id="slide-nav"
-      role="navigation"
-      aria-label="Main Navigation"
-    >
-      <div className="container">
-        <div id="slidemenu">
-          <ul className="justify-around py-10 nav md:flex navbar-nav">
-            {makeNavigationMenu(NAVIGATION_MENU_MAP, router.asPath)}
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Popover className="relative">
+      {({ open }) => (
+        <>
+          <Popover.Button
+            className={classNames(
+              open ? "text-gray-900" : "text-gray-500",
+              "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            )}
+          >
+            <span>Solutions</span>
+            <ChevronDownIcon
+              className={classNames(
+                open ? "text-gray-600" : "text-gray-400",
+                "ml-2 h-5 w-5 group-hover:text-gray-500"
+              )}
+              aria-hidden="true"
+            />
+          </Popover.Button>
+
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel
+              static
+              className="absolute z-10 w-screen max-w-xs px-2 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0"
+            >
+              <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="relative grid gap-6 px-5 py-6 bg-white sm:gap-8 sm:p-8">
+                  {solutions.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="block p-3 -m-3 transition duration-150 ease-in-out rounded-md hover:bg-gray-50"
+                    >
+                      <p className="text-base font-medium text-gray-900">
+                        {item.name}
+                      </p>
+                      <p className="mt-1 text-sm text-gray-500">
+                        {item.description}
+                      </p>
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
+    </Popover>
   )
 }
