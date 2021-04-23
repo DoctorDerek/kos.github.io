2d1640d39940d607c3aa0117d2abbca6266ed9ca
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-23 16:46:16 -0500

    Remove unused comments (re: scripts)

--- pages/index.js
+++ pages/index.js
@@ -23,41 +23,8 @@ export async function getStaticProps() {
   return { props: { posts } }
 }
 
-/*
-import { useEffect } from "react"
-
-const useScript = (url) => {
-  useEffect(() => {
-    const script = document.createElement("script")
-
-    script.src = url
-    script.async = true
-
-    document.body.appendChild(script)
-
-    return () => {
-      document.body.removeChild(script)
-    }
-  }, [url])
-}
-*/
-
 //export default function Home({ posts }) {
 export default function Home() {
-  /*
-  useScript("js/jquery.js")
-  useScript("js/plugins/bootstrap.min.js")
-  useScript("js/plugins/jquery.nivo.slider.js")
-  useScript("js/plugins/slick.min.js")
-  useScript("js/plugins/jquery.magnific-popup.min.js")
-  useScript("js/plugins/jquery.waypoints.min.js")
-  useScript("js/plugins/jquery.countTo.js")
-  useScript("js/plugins/jquery.form.js")
-  useScript("js/plugins/jquery.validate.min.js")
-  useScript("js/custom.js")
-  useScript("js/accessibilitymenu.js")
-  */
-
   return (
     <>
       <Slideshow />
