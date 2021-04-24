c7f40cd1abaf27b92b12039e75b3d1245d5b103d
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-24 00:49:25 -0500

    WIP revert

--- components/DropdownMenu.js
+++ components/DropdownMenu.js
@@ -1,12 +1,35 @@
-import { Fragment } from "react"
-import { Popover, Transition } from "@headlessui/react"
-import { ChevronDownIcon } from "@heroicons/react/solid"
+/*
+import { useRouter } from "next/router"
 import useDropdownMenu from "react-accessible-dropdown-menu-hook"
 import Link from "@/components/Link"
 import DownArrow from "@/data/material-icons/keyboard_arrow_down_black_48dp.svg"
 import LeftArrow from "@/data/material-icons/keyboard_arrow_left_black_48dp.svg"
 import RightArrow from "@/data/material-icons/keyboard_arrow_right_black_48dp.svg"
-import UpArrow from "@/data/material-icons/keyboard_arrow_up_black_48dp.svg"
+import UpArrow from "@/data/material-icons/keyboard_arrow_up_black_48dp.svg"*/
+import FlyoutMenuFullWidth from "@/components/FlyoutMenuFullWidth"
+import FlyoutMenuSimple from "@/components/FlyoutMenuSimple"
+import {
+  ChartBarIcon,
+  CheckCircleIcon,
+  CursorClickIcon,
+  PhoneIcon,
+  PlayIcon,
+  ShieldCheckIcon,
+  ViewGridIcon,
+} from "@heroicons/react/outline"
+import { useState } from "react"
+import { Popover } from "@headlessui/react"
+import { usePopper } from "react-popper"
+
+// lookup RegExp objects to match subpages from current URL href (router.asPath)
+const MENU_LOOKUP_ALIASES = new Map([
+  ["Services", [/home/, /business/, /camp/, /avail/, /pay/]],
+  ["Residential", [/home/]],
+  ["Business", [/business/]],
+  ["Hosting", [/hosting/, /domain/]],
+  ["About", [/news/, /coverage/]],
+  ["My Account", [/support/]],
+])
 
 const NAVIGATION_MENU_MAP = new Map([
   ["Home", "/"],
@@ -14,7 +37,7 @@ const NAVIGATION_MENU_MAP = new Map([
     "Services",
     new Map([
       [
-        "Residential",
+        "Residential Services",
         new Map([
           [
             "Residential Internet",
@@ -93,96 +116,217 @@ const NAVIGATION_MENU_MAP = new Map([
 
 const solutions = [
   {
-    name: "Blog",
-    description: "Learn about tips, product updates and company culture.",
+    name: "Analytics",
+    description:
+      "Get a better understanding of where your traffic is coming from.",
     href: "#",
+    icon: ChartBarIcon,
   },
   {
-    name: "Help Center",
-    description:
-      "Get all of your questions answered in our forums of contact support.",
+    name: "Engagement",
+    description: "Speak directly to your customers in a more meaningful way.",
     href: "#",
-  },
-  {
-    name: "Guides",
-    description:
-      "Learn how to maximize our platform to get the most out of it.",
-    href: "#",
-  },
-  {
-    name: "Events",
-    description:
-      "Check out webinars with experts and learn about our annual conference.",
-    href: "#",
+    icon: CursorClickIcon,
   },
   {
     name: "Security",
-    description: "Understand how we take your privacy seriously.",
+    description: "Your customers' data will be safe and secure.",
     href: "#",
+    icon: ShieldCheckIcon,
+  },
+  {
+    name: "Integrations",
+    description: "Connect with third-party tools that you're already using.",
+    href: "#",
+    icon: ViewGridIcon,
   },
 ]
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ")
-}
-
+/*
 export default function Example() {
-  return (
-    <Popover className="relative">
-      {({ open }) => (
-        <>
-          <Popover.Button
-            className={classNames(
-              open ? "text-gray-900" : "text-gray-500",
-              "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            )}
-          >
-            <span>Solutions</span>
-            <ChevronDownIcon
-              className={classNames(
-                open ? "text-gray-600" : "text-gray-400",
-                "ml-2 h-5 w-5 group-hover:text-gray-500"
-              )}
-              aria-hidden="true"
-            />
-          </Popover.Button>
+  const [referenceElement, setReferenceElement] = useState(null)
+  const [popperElement, setPopperElement] = useState(null)
+  const [arrowElement, setArrowElement] = useState(null)
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    modifiers: [{ name: "arrow", options: { element: arrowElement } }],
+  })
 
-          <Transition
-            show={open}
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
-          >
-            <Popover.Panel
-              static
-              className="absolute z-10 w-screen max-w-xs px-2 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0"
-            >
-              <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                <div className="relative grid gap-6 px-5 py-6 bg-white sm:gap-8 sm:p-8">
-                  {solutions.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="block p-3 -m-3 transition duration-150 ease-in-out rounded-md hover:bg-gray-50"
-                    >
-                      <p className="text-base font-medium text-gray-900">
-                        {item.name}
-                      </p>
-                      <p className="mt-1 text-sm text-gray-500">
-                        {item.description}
-                      </p>
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </Popover.Panel>
-          </Transition>
-        </>
-      )}
+  return (
+    <nav>
+      <button type="button" ref={setReferenceElement}>
+        Reference element
+      </button>
+
+      <div ref={setPopperElement} style={styles.popper} {...attributes.popper}>
+        Popper element
+        <div ref={setArrowElement} style={styles.arrow} />
+      </div>
+    </nav>
+  )
+}*/
+
+/*
+export default function MyPopover() {
+  let [referenceElement, setReferenceElement] = useState()
+  let [popperElement, setPopperElement] = useState()
+  let { styles, attributes } = usePopper(referenceElement, popperElement)
+
+  return (
+    <Popover>
+      <Popover.Button ref={setReferenceElement}>Solutions</Popover.Button>
+
+      <Popover.Panel
+        ref={setPopperElement}
+        style={styles.popper}
+        {...attributes.popper}
+      ></Popover.Panel>
     </Popover>
   )
+}*/
+
+export default function DropdownMenu() {
+  return (
+    <>
+      <Popover.Group as="nav">
+        <div className="block">
+          <div className="w-[600px] flex-shrink-0">
+            <FlyoutMenuFullWidth title="Solutions" menuItems={solutions} />
+          </div>
+          <FlyoutMenuFullWidth title="Solutions" menuItems={solutions} />
+          <FlyoutMenuSimple />
+          <FlyoutMenuSimple />
+        </div>
+      </Popover.Group>
+    </>
+  )
 }
+
+/*
+export default function DropdownMenu() {
+  // fetch current page URL
+  const router = useRouter() // next/router
+  const currentURL = router.asPath
+
+  const isRequired = () => {
+    throw new Error("Missing navigationMenu argument in DropdownMenu")
+  }
+
+  const makeNavigationMenu = (
+    navigationMenu = isRequired(),
+    dropdownMenuItemProps = null
+  ) => {
+    return [...navigationMenu.entries()].map(
+      ([text, destinationOrSubmenu], itemIndex) => {
+        // check to see if we're the current page & need to be highlighted
+        const isCurrentPage =
+          router.asPath === href ||
+          MENU_LOOKUP_ALIASES[text].some((regExp) => regExp.exec(currentURL))
+
+        const isCurrentPageProps =
+          router.asPath === href
+            ? { "aria-current": "page", className: "active" }
+            : {}
+
+        if (typeof destinationOrSubmenu === "string") {
+          // we have a URL, so make a <Link> anchor
+          const href = destinationOrSubmenu
+
+          // set up props if we're the current page
+
+          // set up the props we're passing around for accessibility
+          let childProps = {}
+          if (dropdownMenuItemProps && dropdownMenuItemProps[itemIndex]) {
+            // pass in itemProps from react-accessible-dropdown-menu-hook
+            childProps = { ...dropdownMenuItemProps[itemIndex] }
+          }
+          childProps.key = text + href
+
+          return (
+            <li {...childProps} {...isCurrentPageProps}>
+              <Link href={href} className="block">
+                {text}
+              </Link>
+            </li>
+          )
+        }
+        if (
+          destinationOrSubmenu !== null &&
+          typeof destinationOrSubmenu === "object"
+        ) {
+          const submenu = destinationOrSubmenu
+          const numberOfItems = submenu.length
+          const { buttonProps, itemProps, isOpen, setIsOpen } = useDropdownMenu(
+            numberOfItems
+          )
+          let ArrowIcon
+          if (dropdownMenuItemProps) {
+            // we're inside a sub-menu
+            if (isOpen) {
+              ArrowIcon = <LeftArrow />
+            } else {
+              ArrowIcon = <RightArrow />
+            }
+          } else {
+            // we're a top-level menu
+            if (isOpen) {
+              ArrowIcon = <UpArrow />
+            } else {
+              ArrowIcon = <DownArrow />
+            }
+          }
+          const uniqueKey = text + "Menu"
+          return (
+            <li
+              className="relative dropdown"
+              onMouseEnter={() => setIsOpen(true)}
+              onMouseLeave={() => setIsOpen(false)}
+              key={uniqueKey}
+            >
+              <button
+                {...buttonProps}
+                aria-controls={uniqueKey}
+                aria-expanded={isOpen}
+                className="flex w-full"
+              >
+                {text}
+                <div className="grid self-center w-6 h-6 rounded-full fill-current">
+                  {ArrowIcon}
+                </div>
+              </button>
+              <ul
+                className={
+                  "absolute z-50 bg-white dropdown-menu " +
+                  (isOpen ? "visible" : "hidden")
+                }
+                id={uniqueKey}
+              >
+                <li className="dropdown-submenu">
+                  {destinationOrSubmenu &&
+                    makeNavigationMenu(destinationOrSubmenu, itemProps)}
+                </li>
+              </ul>
+            </li>
+          )
+        }
+        throw new Error("Unknown destinationOrSubmenu prop in DropdownMenu")
+      }
+    )
+  }
+  return (
+    <nav
+      className="navbar"
+      id="slide-nav"
+      role="navigation"
+      aria-label="Main Navigation"
+    >
+      <div className="container">
+        <div id="slidemenu">
+          <ul className="justify-around py-10 nav md:flex navbar-nav">
+            {makeNavigationMenu(NAVIGATION_MENU_MAP)}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+*/
