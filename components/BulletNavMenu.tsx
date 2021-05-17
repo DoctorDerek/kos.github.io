9dd45ff98815313c1d8e00c04a0c045986555a20
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-17 16:32:05 -0500

    fix: update nav links for residential

--- components/BulletNavMenu.tsx
+++ components/BulletNavMenu.tsx
@@ -1,40 +1,38 @@
 import RightArrow from "@/data/material-icons/keyboard_arrow_right_black_48dp.svg"
+import NAVIGATION_MENU from "@/data/NAVIGATION_MENU"
+if (NAVIGATION_MENU[1][1][0][0] !== "Residential") {
+  throw new Error(
+    "BulletNavMenu was expecting `Residential` in NAVIGATION_MENU"
+  )
+}
+const bulletsResidential = NAVIGATION_MENU[1][1][0][1] as NAVIGATION_MENU_TYPE[]
+if (NAVIGATION_MENU[1][1][1][0] !== "Business") {
+  throw new Error("BulletNavMenu was expecting `Business` in NAVIGATION_MENU")
+}
+const bulletsBusiness = NAVIGATION_MENU[1][1][1][1] as NAVIGATION_MENU_TYPE[]
 
 const BULLET = () => (
   <div className="inline-block w-3 h-3 text-blue-800 border-current border-solid rounded-full fill-current border-[1px] mx-4">
     <RightArrow />
   </div>
 )
+const formatBullets = (item: any) => {
+  const [title, href] = item as NAVIGATION_MENU_TYPE
+  return (
+    <li key={(title as string) + "BulletNavMenu"}>
+      <BULLET />
+      <a href={href as string}>{title}</a>
+    </li>
+  )
+}
 export function BulletNavMenu(): JSX.Element {
   return (
     <div className="flex flex-col pt-4 md:space-x-16 md:flex-row md:pt-0">
-      <ul>
-        <li>
-          <BULLET />
-          <a href="/res/high-speed-cable/">High Speed Cable</a>
-        </li>
-        <li>
-          <BULLET />
-          <a href="/res/wireless-broadband/">Wireless Broadband</a>
-        </li>
-        <li>
-          <BULLET />
-          <a href="/res/high-speed-dsl/">High Speed DSL</a>
-        </li>
+      <ul key="Left Column">
+        {bulletsResidential.slice(0, 3).map(formatBullets)}
       </ul>
-      <ul>
-        <li>
-          <BULLET />
-          <a href="/res/voip/">VoIP</a>
-        </li>
-        <li>
-          <BULLET />
-          <a href="/res/mail/">Mail</a>
-        </li>
-        <li>
-          <BULLET />
-          <a href="/res/dial-up/">Dial Up</a>
-        </li>
+      <ul key="Right Column">
+        {bulletsResidential.slice(4, 7).map(formatBullets)}
       </ul>
     </div>
   )
