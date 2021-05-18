3a2d213c6479dfdaf3b150896260ed031ff54b5f
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-18 18:40:41 -0500

    Revert "Revert "refactor: add fullWidth prop to InternalTemplate""
    This reverts commit 8c08c049328cffe6e1059b8a159ced131514b292.

--- components/InternalTemplate.tsx
+++ components/InternalTemplate.tsx
@@ -3,15 +3,19 @@ import { PageSeo } from "@/components/SEO"
 import siteMetadata from "@/data/siteMetadata.json"
 import Image from "@/components/CustomImage"
 
+const classNames = (...classes: string[]): string => classes.join(" ")
+
 export function InternalTemplate({
   title,
   slug,
   bulletNavMenu = null,
+  fullWidth = false,
   children,
 }: {
   title: string | JSX.Element
   slug: string
   bulletNavMenu?: JSX.Element | null
+  fullWidth?: boolean
   children: JSX.Element | JSX.Element[]
 }): JSX.Element {
   const isRequired = () => {
@@ -42,7 +46,12 @@ export function InternalTemplate({
         <Image src="/images/footer-bg.jpg" alt="" />
         {/* alt="" acceptable for purely decorative elements */}
       </div>
-      <div className="max-w-xl px-4 py-8 mx-auto md:px-0 xl:py-16 lg:py-14 md:py-12 sm:py-10">
+      <div
+        className={classNames(
+          fullWidth ? "" : "max-w-xl",
+          "px-4 py-8 mx-auto md:px-0 xl:py-16 lg:py-14 md:py-12 sm:py-10"
+        )}
+      >
         <h1 className="text-left color decor">{title}</h1>
         <div className="pb-4 text-center">
           <ImageFixed
