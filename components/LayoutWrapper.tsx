33928a270420dcab8c57e753293d4ae08caa3521
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-18 00:45:54 -0500

    refactor: create LayoutWrapper.tsx

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,7 +1,7 @@
-import Header from "./Header"
-import Footer from "./Footer"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
-const LayoutWrapper = ({ children }) => {
+const LayoutWrapper = ({ children }: { children: any }) => {
   return (
     <>
       <Header />
