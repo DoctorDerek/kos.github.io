99fb6b0b1c2b3af364cb15c73ab37456185d879d
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-18 01:12:51 -0500

    fix: Type error in Card.jsx

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,23 +1,48 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import Link from "next/link"
 
-const CustomLink = ({ href, ...rest }: { href: string }) => {
+const CustomLink = ({
+  href,
+  children,
+  className,
+  ...rest
+}: {
+  href: string
+  children: any
+  className: string
+}) => {
   const isInternalLink = href && href.startsWith("/")
   const isAnchorLink = href && href.startsWith("#")
 
   if (isInternalLink) {
     return (
       <Link href={href}>
-        <a {...rest} />
+        <a {...rest} className={className}>
+          {children}
+        </a>
       </Link>
     )
   }
 
   if (isAnchorLink) {
-    return <a href={href} {...rest} />
+    return (
+      <a href={href} className={className} {...rest}>
+        {children}
+      </a>
+    )
   }
 
-  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
+  return (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+      className={className}
+      {...rest}
+    >
+      {children}
+    </a>
+  )
 }
 
 export default CustomLink
