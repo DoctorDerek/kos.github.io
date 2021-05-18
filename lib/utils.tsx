425d5036c8f15554783bb01091e092378eee5fc8
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-18 01:45:19 -0500

    fix: type errors in utils.tsx

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -1,6 +1,9 @@
-export const kebabCase = (str) =>
-  str &&
-  str
-    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-    .map((x) => x.toLowerCase())
-    .join("-")
+export const kebabCase = (str: string) => {
+  const result =
+    str &&
+    str.match(
+      /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
+    )
+
+  return result && result.map((x: string) => x.toLowerCase()).join("-")
+}
