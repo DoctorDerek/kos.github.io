d21c411f400b17578a49f64efc5f2563f7f7ee29
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-18 01:44:57 -0500

    fix: type errors in tags.tsx

--- lib/tags.tsx
+++ lib/tags.tsx
@@ -5,21 +5,21 @@ import { kebabCase } from "./utils"
 
 const root = process.cwd()
 
-export async function getAllTags(type) {
+export async function getAllTags(type: string) {
   const files = fs.readdirSync(path.join(root, "data", type))
 
-  let tagCount = {}
+  let tagCount = new Map()
   // Iterate through each post, putting all found tags into `tags`
   files.forEach((file) => {
     const source = fs.readFileSync(path.join(root, "data", type, file), "utf8")
     const { data } = matter(source)
     if (data.tags && data.draft !== true) {
-      data.tags.forEach((tag) => {
-        const formattedTag = kebabCase(tag)
+      data.tags.forEach((tag: string) => {
+        const formattedTag = kebabCase(tag) as string
         if (formattedTag in tagCount) {
-          tagCount[formattedTag] += 1
+          tagCount.set(formattedTag, tagCount.get(formattedTag) + 1)
         } else {
-          tagCount[formattedTag] = 1
+          tagCount.set(formattedTag, 1)
         }
       })
     }
