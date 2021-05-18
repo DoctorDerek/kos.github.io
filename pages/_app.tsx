5fbbfcdb8944ca2d4aea133de2da735f865a5ff4
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-18 01:48:43 -0500

    fix: type errors in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "@/css/tailwind.css"
 
+//@ts-expect-error
 import { MDXProvider } from "@mdx-js/react"
+// Could not find a declaration file for module '@mdx-js/react'.
+
 import { ThemeProvider } from "next-themes"
 import { DefaultSeo } from "next-seo"
 import Head from "next/head"
@@ -8,8 +11,15 @@ import Head from "next/head"
 import { SEO } from "@/components/SEO"
 import LayoutWrapper from "@/components/LayoutWrapper"
 import MDXComponents from "@/components/MDXComponents"
+import React from "react"
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps,
+}: {
+  Component: React.FunctionComponent
+  pageProps: any
+}) {
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
