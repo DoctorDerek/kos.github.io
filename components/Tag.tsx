9b579d68f12e41559762c15b31d29f27b6114edd
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-18 00:48:54 -0500

    refactor: create Tag.tsx

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import { kebabCase } from "@/lib/utils"
 
-const Tag = ({ text }) => {
+const Tag = ({ text }: { text: string }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
       <a className="mr-3 text-sm font-medium text-blue-500 uppercase hover:text-blue-600 dark:hover:text-blue-400">
