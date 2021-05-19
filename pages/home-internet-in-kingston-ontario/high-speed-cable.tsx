ee8f721e8240b560b0991495691ca5400b1ded62
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-18 19:07:06 -0500

    refactor: replace <iframe> and create form calling dslavail-check.php in high-speed-cable.tsx

--- pages/home-internet-in-kingston-ontario/high-speed-cable.tsx
+++ pages/home-internet-in-kingston-ontario/high-speed-cable.tsx
@@ -33,12 +33,39 @@ function HighSpeedCableContent() {
         your needs.
       </span>
       <div className="">
-        <iframe
-          className="w-full h-16 max-w-xl mx-auto border border-black border-solid shadow"
-          scrolling="no"
-          src="https://kos.net/dslavail/dslavail.php"
-          title="Postal Code Search"
-        />
+        <form
+          action="https://kos.net/dslavail/dslavail-check.php"
+          method="post"
+          name="dslcheck"
+          className="flex"
+        >
+          <input type="hidden" name="minidslc" value="1" />
+          <input type="hidden" name="docheck" value="1" />
+          <div className="flex flex-col px-2 pt-1 pb-2 mx-auto space-y-1 text-center border border-black border-solid shadow">
+            <label htmlFor="postalcode" className="text-lg">
+              Enter your Postal Code to check for services available in your
+              area:
+            </label>
+            <div className="space-x-2">
+              <input
+                className="px-1 py-0 text-center"
+                type="text"
+                placeholder="K7K 5W7"
+                name="postalcode"
+                maxLength={7}
+                size={7}
+                id="postalcode"
+              />
+              <input
+                type="submit"
+                value="Check"
+                name="sub"
+                id="sub"
+                className="px-1 text-white border border-black border-solid shadow-sm from-blue-600 bg-gradient-to-b to-blue-800"
+              />
+            </div>
+          </div>
+        </form>
       </div>
       <div className="flex flex-wrap">
         <div className="w-1/3">
