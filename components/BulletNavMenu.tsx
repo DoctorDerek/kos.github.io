5126c9c592e156da641d91c697e3020dcb555b39
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-19 17:03:15 -0500

    feat: add highlighting to current page & hover effect in BulletNavMenu

--- components/BulletNavMenu.tsx
+++ components/BulletNavMenu.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react"
+import { useCurrentPath } from "@/lib/utils"
 import RightArrow from "@/data/material-icons/keyboard_arrow_right_black_48dp.svg"
+import { CheckIcon } from "@heroicons/react/solid"
 import NAVIGATION_MENU from "@/data/NAVIGATION_MENU"
+
 if (NAVIGATION_MENU[1][1][0][0] !== "Residential") {
   throw new Error(
     "BulletNavMenu was expecting `Residential` in NAVIGATION_MENU"
@@ -12,17 +16,45 @@ if (NAVIGATION_MENU[1][1][1][0] !== "Business") {
 const bulletsBusiness = NAVIGATION_MENU[1][1][1][1] as NAVIGATION_MENU_TYPE[]
 const bullets = { Business: bulletsBusiness, Residential: bulletsResidential }
 
-const BULLET = () => (
-  <div className="inline-block w-3 h-3 text-blue-800 border-current border-solid rounded-full fill-current border-[1px] mx-4">
-    <RightArrow />
+const classNames = (...classes: string[]) => classes.join(" ")
+
+const BULLET = ({ hover = false }: { hover?: boolean }) => (
+  <div
+    className={classNames(
+      "inline-block w-3 h-3 border-solid rounded-full fill-current border mx-2",
+      hover
+        ? "bg-blue-800 text-white border-blue-800"
+        : "bg-transparent text-blue-800 border-blue-800"
+    )}
+  >
+    {hover ? (
+      <CheckIcon aria-label="Current page" />
+    ) : (
+      <RightArrow aria-hidden="true" />
+    )}
   </div>
 )
-const formatBullets = (item: any) => {
+const FormatBullet = ({ item }: { item: NAVIGATION_MENU_TYPE }) => {
   const [title, href] = item as NAVIGATION_MENU_TYPE
+  const isCurrentPage = href === useCurrentPath()
+  const [hover, setHover] = useState(false)
   return (
-    <li key={(title as string) + "BulletNavMenu"}>
-      <BULLET />
-      <a href={href as string}>{title}</a>
+    <li
+      key={(title as string) + "BulletNavMenu"}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <BULLET hover={hover || isCurrentPage} />
+      <a
+        href={href as string}
+        className={classNames(
+          hover || isCurrentPage
+            ? "transition transform translate-x-2 underline"
+            : ""
+        )}
+      >
+        {title}
+      </a>
     </li>
   )
 }
@@ -33,8 +65,16 @@ export function BulletNavMenu({
 }): JSX.Element {
   return (
     <div className="flex flex-col pt-4 md:space-x-16 md:flex-row md:pt-0">
-      <ul key="Left Column">{bullets[type].slice(0, 3).map(formatBullets)}</ul>
-      <ul key="Right Column">{bullets[type].slice(4, 7).map(formatBullets)}</ul>
+      <ul key="Left Column">
+        {bullets[type].slice(0, 3).map((item) => (
+          <FormatBullet item={item} key={Math.random()} />
+        ))}
+      </ul>
+      <ul key="Right Column">
+        {bullets[type].slice(4, 7).map((item) => (
+          <FormatBullet item={item} key={Math.random()} />
+        ))}
+      </ul>
     </div>
   )
 }
