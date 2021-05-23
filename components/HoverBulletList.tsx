e3f10488b0149411ed179e8d0942745af1443007
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-23 01:45:26 -0500

    refactor: have HoverBulletList support either <string> or hoverBulletNavMenu props

--- components/HoverBulletList.tsx
+++ components/HoverBulletList.tsx
@@ -1,17 +1,33 @@
 import { Fragment } from "react"
 import HoverBullet from "@/components/HoverBullet"
 export default function HoverBulletList({
-  hoverBullets,
+  hoverBulletStrings = null,
+  hoverBulletNavMenu = null,
 }: {
-  hoverBullets: string[]
+  hoverBulletStrings?: string[] | null
+  hoverBulletNavMenu?: NAVIGATION_MENU_TYPE[] | null
 }) {
+  if (
+    !(hoverBulletStrings || hoverBulletNavMenu) ||
+    (hoverBulletStrings && hoverBulletNavMenu)
+  )
+    throw new Error(
+      "<HoverBulletList> needs one of either hoverBulletStrings or hoverBulletNavMenu props, but not both"
+    )
   return (
     <ul className="mt-4 text-sm sm:text-base md:text-lg lg:text-xl">
-      {hoverBullets.map((text: string) => (
-        <Fragment key={text}>
-          <HoverBullet text={text} />
-        </Fragment>
-      ))}
+      {hoverBulletStrings &&
+        hoverBulletStrings.map((text: string) => (
+          <Fragment key={text}>
+            <HoverBullet text={text} />
+          </Fragment>
+        ))}
+      {hoverBulletNavMenu &&
+        hoverBulletNavMenu.map((text: string) => (
+          <Fragment key={text}>
+            <HoverBullet text={text} />
+          </Fragment>
+        ))}
     </ul>
   )
 }
