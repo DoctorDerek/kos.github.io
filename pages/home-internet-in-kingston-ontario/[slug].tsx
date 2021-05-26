bb558175a3c9bb0c743fb0ad1a72ea735e66ddf6
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-25 20:24:29 -0500

    feat: create [slug].tsx for WIP residential template

---
+++ pages/home-internet-in-kingston-ontario/[slug].tsx
@@ -0,0 +1,77 @@
+import fs from "fs"
+import hydrate from "next-mdx-remote/hydrate"
+import {
+  getFiles,
+  getFileBySlug,
+  getAllFilesFrontMatter,
+  formatSlug,
+} from "@/lib/mdx"
+import PostLayout from "@/layouts/PostLayout"
+import MDXComponents from "@/components/MDXComponents"
+import PageTitle from "@/components/PageTitle"
+import generateRss from "@/lib/generate-rss"
+
+const PATH = "home-internet-in-kingston-ontario"
+
+export async function getStaticPaths() {
+  const posts = await getFiles(PATH)
+
+  return {
+    paths: posts.map((p) => ({
+      params: {
+        slug: formatSlug(p),
+      },
+    })),
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }: { params: any }) {
+  const allPosts = await getAllFilesFrontMatter(PATH)
+  const postIndex =
+    allPosts.findIndex &&
+    allPosts.findIndex((post: FrontMatter) => post.slug === params.slug)
+  const prev = allPosts[postIndex + 1] || null
+  const next = allPosts[postIndex - 1] || null
+  const post = await getFileBySlug(PATH, params.slug)
+
+  // rss
+  const rss = generateRss(allPosts)
+  fs.writeFileSync("./public/index.xml", rss)
+
+  return { props: { post, prev, next } }
+}
+
+export default function Blog({
+  post,
+  prev,
+  next,
+}: {
+  post: any
+  prev: any
+  next: any
+}) {
+  const { mdxSource, frontMatter } = post
+  const content = hydrate(mdxSource, {
+    components: MDXComponents,
+  })
+
+  return (
+    <>
+      {frontMatter.draft !== true ? (
+        <PostLayout frontMatter={frontMatter} prev={prev} next={next}>
+          {content}
+        </PostLayout>
+      ) : (
+        <div className="mt-24 text-center">
+          <PageTitle>
+            Under Construction{" "}
+            <span role="img" aria-label="roadwork sign">
+              🚧
+            </span>
+          </PageTitle>
+        </div>
+      )}
+    </>
+  )
+}
