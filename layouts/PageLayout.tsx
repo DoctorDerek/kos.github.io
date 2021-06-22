8befb9f07ace1c78a7443306cae4757d6227ad53
Dr. Derek Austin <derekraustin@gmail.com>
2021-06-22 17:50:13 -0500

    fix: support {before} in pricingPackagesSectionDetailsPromotion in <PageLayout>

--- layouts/PageLayout.tsx
+++ layouts/PageLayout.tsx
@@ -169,16 +169,25 @@ export default function PageLayout({
 
   function PricingPackagesSectionDetailsPromotion() {
     if (pricingPackagesSectionDetailsPromotion) {
-      const [, dollars, cents, footnotes, duration] =
+      const [match, dollars, cents, footnotes, duration] =
         extractDollarsCentsAndFootnotesFromPrice(
           pricingPackagesSectionDetailsPromotion
         )
+      let beforeMatch = ""
+      if (match !== duration) {
+        // we found a valid price match
+        const beforeMatchArray = /(.*)\$(\d+)\.?(\d+)?\^?([\d,]+)?(.+)?/.exec(
+          pricingPackagesSectionDetailsPromotion
+        )
+        // separate out the before subgroup i.e. beforeMatchArray[1], if any
+        beforeMatch = beforeMatchArray ? beforeMatchArray[1] : ""
+      }
       return (
         <>
           <div className="max-w-6xl mx-auto mb-12">
             <HoverBox>
               <div className="max-w-3xl px-4 mx-auto text-3xl font-bold text-center text-red-brand">
-                ADD HOME PHONE SERVICE TO ANY HIGH SPEED INTERNET PACKAGE FOR
+                {beforeMatch}
                 <span className="text-black">
                   {" "}
                   {dollars && (
