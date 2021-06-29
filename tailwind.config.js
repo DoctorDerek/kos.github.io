41a0498c4b0df865712ba993358b44efc87e5e10
Dr. Derek Austin <derekraustin@gmail.com>
2021-06-28 20:25:20 -0500

    chore: delete unused dark theme in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -185,59 +185,6 @@ module.exports = {
             },
           },
         },
-        dark: {
-          css: {
-            color: theme("colors.gray.300"),
-            a: {
-              color: theme("colors.blue.500"),
-              "&:hover": {
-                color: theme("colors.blue.400"),
-              },
-              code: { color: theme("colors.blue.400") },
-            },
-            h1: {
-              fontWeight: "700",
-              letterSpacing: theme("letterSpacing.tight"),
-              color: theme("colors.gray.100"),
-            },
-            h2: {
-              fontWeight: "700",
-              letterSpacing: theme("letterSpacing.tight"),
-              color: theme("colors.gray.100"),
-            },
-            h3: {
-              fontWeight: "600",
-              color: theme("colors.gray.100"),
-            },
-            "h4,h5,h6": {
-              color: theme("colors.gray.100"),
-            },
-            code: {
-              backgroundColor: theme("colors.gray.800"),
-            },
-            hr: { borderColor: theme("colors.gray.700") },
-            "ol li:before": {
-              fontWeight: "600",
-              color: theme("colors.gray.400"),
-            },
-            "ul li:before": {
-              backgroundColor: theme("colors.gray.400"),
-            },
-            strong: { color: theme("colors.gray.100") },
-            thead: {
-              color: theme("colors.gray.100"),
-            },
-            tbody: {
-              tr: {
-                borderBottomColor: theme("colors.gray.700"),
-              },
-            },
-            blockquote: {
-              color: theme("colors.gray.100"),
-              borderLeftColor: theme("colors.gray.700"),
-            },
-          },
-        },
       }),
     },
   },
