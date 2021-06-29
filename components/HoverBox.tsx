048311b53f4398aa6ce9201000d4e0db575be34a
Dr. Derek Austin <derekraustin@gmail.com>
2021-06-29 18:29:40 -0500

    refactor: improve readability of <HoverBox>

--- components/HoverBox.tsx
+++ components/HoverBox.tsx
@@ -11,7 +11,9 @@ export default function HoverBox({
     <div
       className={classNames(
         className,
-        "transition duration-500 border border-gray-300 border-solid hover:shadow-md"
+        "transition duration-500",
+        "border border-gray-300 border-solid",
+        "hover:shadow-md"
       )}
     >
       <div className="flex flex-col py-4 space-y-2 transition duration-500 border-t-4 border-transparent border-solid hover:shadow-sm hover:border-blue-brand">
