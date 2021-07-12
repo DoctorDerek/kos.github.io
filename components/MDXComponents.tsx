60b4a9d434ff2f1551d5bad340734b3da9093496
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-12 17:23:15 -0500

    feat: support <DynamicImage> in Markdown files

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,14 +1,12 @@
-import ImageFixed from "next/image"
-
-import Image from "@/components/CustomImage"
 import CustomLink from "@/components/CustomLink"
+import DynamicImage from "@/components/DynamicImage"
 
 const MDXComponents = {
-  Image,
   a: CustomLink,
   Link: CustomLink,
-  img: ImageFixed,
-  ImageFixed: ImageFixed,
+  img: DynamicImage,
+  Image: DynamicImage,
+  ImageFixed: DynamicImage,
 }
 
 export default MDXComponents
