121c6c0f42982a60aa5c2ad5bd5c624037d4a309
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-13 00:09:02 -0500

    chore: add next 11 types to next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
 declare module "*.svg" {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
   export default content
