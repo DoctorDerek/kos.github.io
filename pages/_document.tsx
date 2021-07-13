4195abc770ebb59a560adbfef6bdbb78c45ded71
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-13 00:45:45 -0500

    fix: condense google fonts call into one line

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,11 +34,7 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Lato"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans"
+            href="https://fonts.googleapis.com/css2?family=Lato|Open+Sans"
             rel="stylesheet"
           />
         </Head>
