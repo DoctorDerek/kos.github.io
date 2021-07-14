5398f721d834fb6962d42001f34162ab8aacd7ef
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-13 19:25:26 -0500

    fix: change "mb-4" to "space-y-4"

--- components/IconBadge.tsx
+++ components/IconBadge.tsx
@@ -28,7 +28,7 @@ export default function IconBadge({
   return (
     <div
       className={classNames(
-        "pl-4 pr-4 sm:w-1/3 mb-6 text-center",
+        "pl-4 pr-4 sm:w-1/3 mb-6 text-center space-y-4",
         size === "large" ? "sm:w-1/2" : "sm:w-1/3",
         uppercase ? "uppercase" : ""
       )}
@@ -36,12 +36,7 @@ export default function IconBadge({
       <div className="flex items-center justify-center w-20 h-20 mx-auto text-xl font-bold text-white rounded-full fill-current bg-gradient-to-b to-blue-light from-teal-brand hover:to-blue-800 transform-gpu">
         <div className="w-12 h-12">{<HeroIconComponent />}</div>
       </div>
-      <h5
-        className={classNames(
-          "mb-4 text-gray-900",
-          uppercase ? "uppercase" : ""
-        )}
-      >
+      <h5 className={classNames("text-gray-900", uppercase ? "uppercase" : "")}>
         {label.map((line) => (
           <div key={line}>{line}</div>
         ))}
