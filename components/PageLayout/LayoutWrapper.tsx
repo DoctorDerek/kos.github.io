9956dda3b5f1932962e394be01ddcb895164ca12
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-16 18:19:51 -0500

    chore: delete unnecessary #page-content CSS

--- components/PageLayout/LayoutWrapper.tsx
+++ components/PageLayout/LayoutWrapper.tsx
@@ -5,7 +5,7 @@ const LayoutWrapper = ({ children }: { children?: any }) => {
   return (
     <>
       <Header />
-      <div id="page-content">{children}</div>
+      {children}
       <Footer />
     </>
   )
