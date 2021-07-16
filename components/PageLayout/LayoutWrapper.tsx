f3d020351a24e45b446ac5876ebeb21421d56cc4
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-16 18:22:46 -0500

    refactor: move <body> styles to <LayoutWrapper>

--- components/PageLayout/LayoutWrapper.tsx
+++ components/PageLayout/LayoutWrapper.tsx
@@ -3,11 +3,11 @@ import Header from "@/components/PageLayout/Header"
 
 const LayoutWrapper = ({ children }: { children?: any }) => {
   return (
-    <>
+    <div className="text-lg leading-5 text-black bg-white">
       <Header />
       {children}
       <Footer />
-    </>
+    </div>
   )
 }
 
