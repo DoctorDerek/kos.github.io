680d6d1c614bfd327ed600c35a5a8c4419bf9f27
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-17 18:23:26 -0500

    fix: correct spacing and hover effects for <HomeNewsAndEventsItem>

--- components/Home/HomeNewsAndEvents.tsx
+++ components/Home/HomeNewsAndEvents.tsx
@@ -12,7 +12,7 @@ export default function HomeNewsAndEvents() {
           News and Events
           <DIVIDER />
         </h2>
-        <div className="flex justify-evenly">
+        <div className="flex mx-4 space-x-4 justify-evenly">
           <HomeNewsAndEventsItem />
           <HomeNewsAndEventsItem />
           <HomeNewsAndEventsItem />
@@ -23,19 +23,22 @@ export default function HomeNewsAndEvents() {
 
   function HomeNewsAndEventsItem() {
     return (
-      <div className="flex flex-col p-4 mx-4 shadow-outer group rounded-2xl">
+      <div className="relative flex flex-col p-4 shadow-outer group rounded-2xl">
         <h5 className="mb-2 text-blue-brand">
           Fibre cut causing major outage **RESOLVED**
         </h5>
         <p className="pr-4 mb-3">
           Today's outage was resolved at 6:15pm. All towers are back online and
           are fully functional.
-          <Link href="/news/events/#57" className="">
+          <Link href="/news/events/#57" className="group-hover:underline">
             more...
           </Link>
         </p>
-        <div className="absolute w-8 h-8 transition-all duration-500 border-2 border-solid rounded-full fill-curren right-4 bottom-4 group-hover:text-white group-hover:bg-blue-brand group-hover:border-blue-brand border-blue-brand text-blue-brand">
-          <Link href="/news/events/#57">
+        <div className="absolute w-8 h-8 transition-all duration-500 border-2 border-solid rounded-full right-4 bottom-4 group-hover:bg-blue-brand border-blue-brand">
+          <Link
+            href="/news/events/#57"
+            className="fill-current group-hover:text-white text-blue-brand"
+          >
             <ChevronRightIconComponent />
           </Link>
         </div>
