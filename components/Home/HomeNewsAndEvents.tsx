f65463c167ad0912141f6b3051f5e795b0442dad
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-17 18:24:04 -0500

    feat: add <Link> surrounding entire <HomeNewwsAndEventsItem>

--- components/Home/HomeNewsAndEvents.tsx
+++ components/Home/HomeNewsAndEvents.tsx
@@ -23,26 +23,28 @@ export default function HomeNewsAndEvents() {
 
   function HomeNewsAndEventsItem() {
     return (
-      <div className="relative flex flex-col p-4 shadow-outer group rounded-2xl">
-        <h5 className="mb-2 text-blue-brand">
-          Fibre cut causing major outage **RESOLVED**
-        </h5>
-        <p className="pr-4 mb-3">
-          Today's outage was resolved at 6:15pm. All towers are back online and
-          are fully functional.
-          <Link href="/news/events/#57" className="group-hover:underline">
-            more...
-          </Link>
-        </p>
-        <div className="absolute w-8 h-8 transition-all duration-500 border-2 border-solid rounded-full right-4 bottom-4 group-hover:bg-blue-brand border-blue-brand">
-          <Link
-            href="/news/events/#57"
-            className="fill-current group-hover:text-white text-blue-brand"
-          >
-            <ChevronRightIconComponent />
-          </Link>
+      <Link href="/news/events/#57" className="text-black hover:no-underline">
+        <div className="relative flex flex-col p-4 shadow-outer group rounded-2xl">
+          <h5 className="mb-2 text-blue-brand">
+            Fibre cut causing major outage **RESOLVED**
+          </h5>
+          <p className="pr-4 mb-3">
+            Today's outage was resolved at 6:15pm. All towers are back online
+            and are fully functional.
+            <Link href="/news/events/#57" className="group-hover:underline">
+              more...
+            </Link>
+          </p>
+          <div className="absolute w-8 h-8 transition-all duration-500 border-2 border-solid rounded-full right-4 bottom-4 group-hover:bg-blue-brand border-blue-brand">
+            <Link
+              href="/news/events/#57"
+              className="fill-current group-hover:text-white text-blue-brand"
+            >
+              <ChevronRightIconComponent />
+            </Link>
+          </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
