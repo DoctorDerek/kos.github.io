272138e9e720668856f9441d46789b9119de765e
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-16 22:28:41 -0500

    refactor: create <HomeNewsAndEventsItem>

--- components/Home/HomeNewsAndEvents.tsx
+++ components/Home/HomeNewsAndEvents.tsx
@@ -11,110 +11,49 @@ export default function HomeNewsAndEvents() {
           <DIVIDER />
         </h2>
         <div className="flex flex-wrap news-item-row">
-          <div className="pl-4 pr-4 sm:w-1/2 md:w-1/3">
-            <div
-              className="news-item"
-              data-animation="fadeInLeft"
-              data-animation-delay="0s"
-            >
-              <div className="news-item-image">
-                <div className="grid w-full h-40">
-                  <Image src="/assets/images/news/logo.png" alt="Decoration" />
-                </div>
-              </div>
-              <div className="news-item-text">
-                <h5 className="news-item-title">
-                  Fibre cut causing major outage **RESOLVED**
-                </h5>
-                <p>
-                  Today's outage was resolved at 6:15pm. All towers are back
-                  online and are fully functional.
-                  <Link
-                    href="/news/events/#57"
-                    className="underline text-blue-brand"
-                  >
-                    more...
-                  </Link>
-                </p>
-                <span className="news-item-more-link">
-                  <Link href="/news/events/#57">
-                    <span className="flex-wrap icon icon-right-arrow" />
-                  </Link>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="pl-4 pr-4 sm:w-1/2 md:w-1/3">
-            <div
-              className="news-item"
-              data-animation="fadeInLeft"
-              data-animation-delay="0s"
-            >
-              <div className="news-item-image">
-                <div className="grid w-full h-40">
-                  <Image src="/assets/images/news/logo.png" alt="Decoration" />
-                </div>
-              </div>
-              <div className="news-item-text">
-                <h5 className="news-item-title">COVID-19</h5>
-                <p>
-                  With the significant and growing concern across North America
-                  of the COVID-19 (“coronavirus”) situation, we wishto provide
-                  you on an update on how KOS is handling this situation as it
-                  relates to our customers and employees.We will remain fully
-                  operational.
-                  <Link
-                    href="/news/events/#51"
-                    className="underline text-blue-brand"
-                  >
-                    more...
-                  </Link>
-                </p>
-                <span className="news-item-more-link">
-                  <Link href="/news/events/#51">
-                    <span className="flex-wrap icon icon-right-arrow" />
-                  </Link>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="pl-4 pr-4 sm:w-1/2 md:w-1/3">
-            <div
-              className="news-item"
-              data-animation="fadeInLeft"
-              data-animation-delay="0s"
-            >
-              <div className="news-item-image">
-                <div className="grid w-full h-40">
-                  <Image
-                    src="/assets/images/news/814185689_news-unlimited-packages.jpg"
-                    alt="Decoration"
-                  />
-                </div>
-              </div>
-              <div className="news-item-text">
-                <h5 className="news-item-title">
-                  Unlimited Packages Now Available
-                </h5>
-                <p>
-                  Enjoy unlimited data on our Cable and DSL services.
-                  <Link
-                    href="/news/events/#21"
-                    className="underline text-blue-brand"
-                  >
-                    more...
-                  </Link>
-                </p>
-                <span className="news-item-more-link">
-                  <Link href="/news/events/#21">
-                    <span className="flex-wrap icon icon-right-arrow" />
-                  </Link>
-                </span>
-              </div>
-            </div>
-          </div>
+          <HomeNewsAndEventsItem />
+          <HomeNewsAndEventsItem />
+          <HomeNewsAndEventsItem />
         </div>
       </div>
     </div>
   )
+
+  function HomeNewsAndEventsItem() {
+    return (
+      <div className="pl-4 pr-4 sm:w-1/2 md:w-1/3">
+        <div
+          className="news-item"
+          data-animation="fadeInLeft"
+          data-animation-delay="0s"
+        >
+          <div className="news-item-image">
+            <div className="grid w-full h-40">
+              <Image src="/assets/images/news/logo.png" alt="Decoration" />
+            </div>
+          </div>
+          <div className="news-item-text">
+            <h5 className="news-item-title">
+              Fibre cut causing major outage **RESOLVED**
+            </h5>
+            <p>
+              Today's outage was resolved at 6:15pm. All towers are back online
+              and are fully functional.
+              <Link
+                href="/news/events/#57"
+                className="underline text-blue-brand"
+              >
+                more...
+              </Link>
+            </p>
+            <span className="news-item-more-link">
+              <Link href="/news/events/#57">
+                <span className="flex-wrap icon icon-right-arrow" />
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
