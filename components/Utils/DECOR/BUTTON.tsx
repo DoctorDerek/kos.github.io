d62a32ae68497f894b73b286648919f78d80ab19
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-18 14:14:48 -0500

    feat: add hover effect with duration-500 to <BUTTON>

--- components/Utils/DECOR/BUTTON.tsx
+++ components/Utils/DECOR/BUTTON.tsx
@@ -5,9 +5,17 @@ export default function BUTTON({ href, text }: { href: string; text: string }) {
     <div className="flex items-center">
       <Link
         href={href}
-        className="relative px-4 py-3 font-bold text-center text-white no-underline uppercase shadow-md select-none font-lato rounded-xl bg-red-button-dark bg-gradient-to-t from-red-button-light to-red-button-dark hover:bg-gradient-to-b"
+        className="relative px-4 py-3 font-bold text-center text-white no-underline uppercase shadow-md select-none font-lato rounded-xl bg-red-button-dark bg-gradient-to-t from-red-button-light to-red-button-dark"
       >
         {text}
+        <div
+          className="absolute w-full h-full translate-x-[-50%] translate-y-[-50%] left-1/2 top-1/2 rounded-xl opacity-100 hover:opacity-0 transition-opacity duration-500 flex items-center justify-center bg-gradient-to-b from-red-button-light to-red-button-dark"
+          aria-hidden="true"
+        >
+          {/* second <div> is necessary for duration-500 hover effect as the */}
+          {/* browser can easily transition opacity but not the bg-gradient */}
+          {text}
+        </div>
       </Link>
     </div>
   )
