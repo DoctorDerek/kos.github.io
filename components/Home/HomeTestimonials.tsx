72a85933b08c9e6dfc5a9af12d9017d443ab03eb
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-18 19:53:25 -0500

    refactor: add testimonialsData to <HomeTestimonials>

--- components/Home/HomeTestimonials.tsx
+++ components/Home/HomeTestimonials.tsx
@@ -43,6 +43,33 @@ export default function Testimonials() {
     }
   }, [pause, slider])
 
+  const testimonialsData = [
+    {
+      testimonial:
+        "I have been a business customer of KOS for over 10 years. I rely on my web site to drive traffic to my business and I depend on KOS to make sure that my site is up and running 24/7. When I do have any technical problems they are only a phone call away. The staff is wonderful to deal with.",
+      author: "Donna",
+      business: "Creative Effects",
+    },
+    {
+      testimonial:
+        "Kingston Online Services is a very forward thinking company and their innovative and up-to-date technology keeps our business running smoothly. We rely on our internet for several applications and any questions or concerns that I have are answered quickly and professionally. Their service people are always very prompt and courteous. We are proud to have KOS as a member of our team and would not hesitate to recommend them to anyone as a high speed internet provider.",
+      author: "Brenda",
+      business: "Chalk Well Drilling",
+    },
+    {
+      testimonial:
+        "Limestone Property Management has been a customer of KOS since 2001. We're very pleased with their fast, friendly and courteous service. We're happy with the services that we have with them, and enjoy partnering with a local company that works hard to support the communities they work in. I would highly recommend that you make KOS your first call if you are looking for internet service options.",
+      author: "Jason Ruffolo",
+      business: "Limestone Property Management (President)",
+    },
+    {
+      testimonial:
+        "We love K.O.S.! We wanted something local, reliable and affordable with no hidden surprises on our bill each month. Kingston Online Services provided all of that and more including fabulous customer service!",
+      author: "Kim and Jim Long",
+      business: "Must Love Chocolate",
+    },
+  ]
+
   const QuoteIcon = () => (
     <div className="grid self-center w-24 h-24 p-1 mx-3 transition-all duration-500 rounded-full fill-current scale-x-[-1] text-blue-brand absolute left-0 top-[-1rem] z-10">
       <FormatQuoteRoundedIcon aria-hidden="true" className="w-full h-full" />
@@ -50,108 +77,28 @@ export default function Testimonials() {
   )
 
   return (
-    <div className="">
-      <div className="mx-auto">
-        <div className="flex flex-wrap">
-          <div className="w-full">
-            <div className="testimonials" data-animation="fadeInLeft">
-              <h3 className="text-center text-blue-brand decor">
-                What our customers are saying
-                <DIVIDER />
-              </h3>
-              <div className="testimonials-carousel">
-                <div
-                  ref={sliderRef as React.RefObject<HTMLDivElement>}
-                  className="keen-slider font-lato"
-                >
-                  <div className="px-6 keen-slider__slide number-slide1">
-                    <QuoteIcon />
-                    <div className="pt-8 mx-5 my-3 text-center">
-                      <div className="relative p-6 font-serif text-lg italic leading-6 text-gray-600 bg-gray-200 md:text-xl md:p-13">
-                        <p>
-                          I have been a business customer of KOS for over 10
-                          years. I rely on my web site to drive traffic to my
-                          business and I depend on KOS to make sure that my site
-                          is up and running 24/7. When I do have any technical
-                          problems they are only a phone call away. The staff is
-                          wonderful to deal with.
-                        </p>
-                      </div>
-                      <div className="mt-10 mb-3 text-sm font-bold text-gray-700 md:text-lg">
-                        Donna,{" "}
-                        <em style={{ fontWeight: 100 }}> Creative Effects</em>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="px-6 keen-slider__slide number-slide2">
-                    <QuoteIcon />
-                    <div className="pt-8 mx-5 my-3 text-center">
-                      <div className="relative p-6 font-serif text-lg italic leading-6 text-gray-600 bg-gray-200 md:text-xl md:p-13">
-                        <p>
-                          Kingston Online Services is a very forward thinking
-                          company and their innovative and up-to-date technology
-                          keeps our business running smoothly.&nbsp; We rely on
-                          our internet for several applications and any
-                          questions or concerns that I have are answered quickly
-                          and professionally.&nbsp; &nbsp;Their service people
-                          are always very prompt and courteous.&nbsp; We are
-                          proud to have KOS as a member of our team and would
-                          not hesitate to recommend them to anyone as a high
-                          speed internet provider.
-                        </p>
-                      </div>
-                      <div className="mt-10 mb-3 text-sm font-bold text-gray-700 md:text-lg">
-                        Brenda,{" "}
-                        <em style={{ fontWeight: 100 }}>Chalk Well Drilling</em>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="px-6 keen-slider__slide number-slide3">
-                    <QuoteIcon />
-                    <div className="pt-8 mx-5 my-3 text-center">
-                      <div className="relative p-6 font-serif text-lg italic leading-6 text-gray-600 bg-gray-200 md:text-xl md:p-13">
-                        <p>
-                          Limestone Property Management has been a customer of
-                          KOS since 2001.&nbsp; We&rsquo;re very pleased with
-                          their fast, friendly and courteous service.&nbsp;
-                          We&rsquo;re happy with the services that we have with
-                          them, and enjoy partnering with a local company that
-                          works hard to support the communities they work
-                          in.&nbsp; I would highly recommend that you make KOS
-                          your first call if you are looking for internet
-                          service options.
-                        </p>
-                      </div>
-                      <div className="mt-10 mb-3 text-sm font-bold text-gray-700 md:text-lg">
-                        Jason Ruffolo,{" "}
-                        <em style={{ fontWeight: 100 }}>
-                          Limestone Property Management (President)
-                        </em>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="px-6 keen-slider__slide number-slide4">
-                    <QuoteIcon />
-                    <div className="pt-8 mx-5 my-3 text-center">
-                      <div className="relative p-6 font-serif text-lg italic leading-6 text-gray-600 bg-gray-200 md:text-xl md:p-13">
-                        <p>
-                          We love K.O.S.! We wanted something local, reliable
-                          and affordable with no hidden surprises on our bill
-                          each month. Kingston Online Services provided all of
-                          that and more including fabulous customer service!
-                        </p>
-                      </div>
-                      <div className="mt-10 mb-3 text-sm font-bold text-gray-700 md:text-lg">
-                        Kim and Jim Long,{" "}
-                        <em style={{ fontWeight: 100 }}>Must Love Chocolate</em>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+    <div className="flex flex-wrap w-full mb-4">
+      <h3 className="mx-auto text-center text-blue-brand">
+        What our customers are saying
+        <DIVIDER />
+      </h3>
+      <div
+        ref={sliderRef as React.RefObject<HTMLDivElement>}
+        className="keen-slider font-lato"
+      >
+        {testimonialsData.map(({ testimonial, author, business }) => (
+          <div className="px-6 keen-slider__slide" key={testimonial}>
+            <QuoteIcon />
+            <div className="pt-8 mx-5 my-3 text-center">
+              <div className="relative p-6 font-serif text-lg italic leading-6 text-gray-600 bg-gray-200 md:text-xl md:p-13 rounded-3xl">
+                {testimonial}
+              </div>
+              <div className="mt-10 mb-3 text-sm font-bold text-gray-700 md:text-lg">
+                {author}, <span className="italic font-thin">{business}</span>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   )
