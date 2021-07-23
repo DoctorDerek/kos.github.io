62e47451294576f1c2f94a3f49147146826334c7
Dr. Derek Austin <derekraustin@gmail.com>
2021-07-22 20:49:39 -0500

    feat: support new props href and color on <IconBadge> to match design specification

--- components/Home/HomeServicesCarousel.tsx
+++ components/Home/HomeServicesCarousel.tsx
@@ -5,17 +5,23 @@ const iconBadgesData: IconBadge[] = [
     icon: "home",
     label: ["Residential", "Services"],
     text: "Stay connected with family",
+    href: "/residential-services",
+    size: "large",
     uppercase: true,
+    color: "gray",
   },
   {
     icon: "briefcase",
     label: ["Business", "Services"],
     text: "Keep your business connected",
+    href: "/business-services",
+    size: "large",
     uppercase: true,
+    color: "gray",
   },
 ]
-export default function HomeServicesCarousel(iconBadges: IconBadge[]) {
-  iconBadges = iconBadgesData
+export default function HomeServicesCarousel() {
+  const iconBadges = iconBadgesData
   return (
     <div className="mx-auto my-20">
       <div className="flex flex-wrap justify-evenly">
--- components/Hover/IconBadge.tsx
+++ components/Hover/IconBadge.tsx
@@ -1,3 +1,4 @@
+import Link from "@/components/Utils/CustomLink"
 import HERO_ICONS from "@/lib/HERO_ICONS"
 import { classNames } from "@/lib/utils"
 
@@ -5,8 +6,10 @@ export default function IconBadge({
   icon,
   label,
   text,
+  href,
   size,
   uppercase,
+  color,
 }: IconBadge) {
   // support string | string[] for convenience by wrapping strings with an array
   if (!Array.isArray(label)) {
@@ -27,32 +30,37 @@ export default function IconBadge({
 
   return (
     <div className="pl-4 pr-4 mb-6 space-y-4 text-center group sm:w-1/3">
-      <div
-        className={classNames(
-          "relative flex items-center justify-center mx-auto rounded-full bg-blue-brand",
-          size ? "w-40 h-40" : "w-24 h-24"
-        )}
-      >
-        <div className="absolute w-full h-full transition-all duration-500 rounded-full bg-gradient-to-b to-blue-light from-teal-brand group-hover:opacity-0" />
-        {/* position: absolute for gradient hover fade-opacity effect */}
+      <LinkWrapper href={href}>
         <div
           className={classNames(
-            "absolute text-white fill-current",
-            size ? "w-24 h-24" : "w-12 h-12"
+            "relative flex items-center justify-center mx-auto rounded-full bg-blue-brand",
+            size ? "w-40 h-40" : "w-24 h-24"
           )}
         >
-          {<HeroIconComponent aria-hidden="true" />}
+          <div className="absolute w-full h-full transition-all duration-500 rounded-full bg-gradient-to-b to-blue-light from-teal-brand group-hover:opacity-0" />
+          {/* position: absolute for gradient hover fade-opacity effect */}
+          <div
+            className={classNames(
+              "absolute text-white fill-current",
+              size ? "w-24 h-24" : "w-12 h-12"
+            )}
+          >
+            {<HeroIconComponent aria-hidden="true" />}
+          </div>
         </div>
-      </div>
+      </LinkWrapper>
       <div
         className={classNames(
-          "p-0 mb-5 text-3xl font-bold uppercase font-lato text-gray-600",
-          uppercase ? "uppercase" : ""
+          "p-0 mb-5 text-3xl font-bold uppercase font-lato",
+          uppercase ? "uppercase" : "",
+          color === "gray" ? "text-gray-600" : "text-black"
         )}
       >
-        {label.map((line) => (
-          <div key={line}>{line}</div>
-        ))}
+        <LinkWrapper href={href}>
+          {label.map((line) => (
+            <div key={line}>{line}</div>
+          ))}
+        </LinkWrapper>
       </div>
       <div className="text-xl">
         {text.map((line) => (
@@ -61,4 +69,29 @@ export default function IconBadge({
       </div>
     </div>
   )
+
+  function LinkWrapper({
+    href,
+    children,
+  }: {
+    href: string | undefined
+    children: React.ReactNode
+  }) {
+    return (
+      <>
+        {href && (
+          <Link
+            href={href}
+            className={classNames(
+              "hover:text-blue-brand hover:no-underline group-hover:text-blue-brand",
+              color === "gray" ? "text-gray-600" : "text-black"
+            )}
+          >
+            {children}
+          </Link>
+        )}
+        {!href && <>{children}</>}
+      </>
+    )
+  }
 }
